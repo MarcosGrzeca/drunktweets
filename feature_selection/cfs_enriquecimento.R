704692f06b7c3a8b4f287d9f64d7a415afac72f3
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-08-05 15:31:29 -0300

    CFS para enriquecimento

--- feature_selection/cfs_enriquecimento.R
+++ feature_selection/cfs_enriquecimento.R
@@ -10,24 +10,12 @@ DATABASE <- "icwsm"
 clearConsole();
 
 dados <- query("SELECT t.id, drunk AS resposta,
-               (
-                 SELECT GROUP_CONCAT(DISTINCT(REPLACE(type, 'http://dbpedia.org/class/', '')))
-                 FROM
-                 (
-                    SELECT c.resource AS resource,
-                    tn.idTweet
-                    FROM semantic_tweets_nlp tn
-                    JOIN semantic_conceito c ON c.palavra = tn.palavra
-                    WHERE c.sucesso = 1
-                    GROUP BY 1,2
-                  ) AS louco
-               JOIN resource_type ty ON ty.resource = louco.resource
-               WHERE louco.idTweet = t.id
-               ) AS resources
-               FROM semantic_tweets_alcolic t
-               WHERE situacao = 1")
-
-save(dados, file = "rdas/cfs.Rda")
+                  (SELECT GROUP_CONCAT(tn.palavra)
+                   FROM semantic_tweets_nlp tn
+                   WHERE tn.idTweet = t.id
+                   GROUP BY t.id) AS entidades
+              FROM semantic_tweets_alcolic t
+              WHERE situacao = 1")
 
 dados$resposta[is.na(dados$resposta)] <- 0
 dados$resposta[dados$resposta == "X"] <- 1
@@ -35,8 +23,9 @@ dados$resposta[dados$resposta == "N"] <- 0
 dados$resposta[dados$resposta == "S"] <- 1
 
 dados$resposta <- as.factor(dados$resposta)
-dados$resources <- enc2utf8(dados$resources)
-dados$resources = gsub("/", "_", dados$resources)
+dados$entidades <- enc2utf8(dados$entidades)
+dados$entidades = gsub(" ", "_", dados$entidades)
+dados$entidades = gsub("/", "__", dados$entidades)
 
 clearConsole()
 
@@ -50,7 +39,7 @@ library(SnowballC)
 setDT(dados)
 setkey(dados, id)
 
-it_train = itoken(strsplit(dados$resources, ","), 
+it_train = itoken(strsplit(dados$entidades, ","), 
                   preprocessor = tolower,
                   tokenizer = word_tokenizer,
                   ids = dados$id, 
@@ -61,8 +50,6 @@ vocab = prune_vocabulary(vocab, term_count_min = 3)
 vectorizer = vocab_vectorizer(vocab)
 dataFrameResource = create_dtm(it_train, vectorizer)
 dataFrameResource <- as.data.frame(as.matrix(dataFrameResource))
-
-#dump(colnames(dataFrameResource), "teste.csv")
 
 library(rowr)
 library(RWeka)
@@ -78,4 +65,4 @@ subset
 f <- as.simple.formula(subset, "resposta")
 print(f)
 
-save.image(file="rdas/cfs.RData")
+save.image(file="rdas/cfs_enriquecimento.RData")
