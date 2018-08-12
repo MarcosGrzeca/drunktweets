66768a91dbb0028e92bc2a52260f27d5514521f5
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-08-12 16:55:33 -0300

    Vamos de novo

---
+++ svm/generate_data_article_sem_entidades.R
@@ -0,0 +1,54 @@
+options(max.print = 99999999)
+
+library(tools)
+library(rowr)
+library(text2vec)
+library(data.table)
+library(SnowballC)
+
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+
+#Configuracoes
+DATABASE <- "icwsm"
+clearConsole();
+
+dados <- getDadosSVM()
+dados$hashtags = gsub("#", "#tag_", dados$hashtags)
+dados$textParser = gsub("'", "", dados$textParser)
+dados$numeroErros[dados$numeroErros > 1] <- 1
+dados <- discretizarTurno(dados)
+clearConsole()
+
+setDT(dados)
+setkey(dados, id)
+
+it_train = itoken(dados$textParser, 
+                  preprocessor = tolower,
+                  tokenizer = word_tokenizer,
+                  ids = dados$id, 
+                  progressbar = TRUE)
+
+vocab = create_vocabulary(it_train, stopwords = tm::stopwords("en"), ngram = c(1L, 2L))
+vocab = prune_vocabulary(vocab, term_count_min = 3)
+vectorizer = vocab_vectorizer(vocab)
+dtm_train_texto = create_dtm(it_train, vectorizer)
+
+it_train_hash = itoken(dados$hashtags, 
+                       preprocessor = tolower, 
+                       tokenizer = word_tokenizer, 
+                       ids = dados$id, 
+                       progressbar = TRUE)
+
+vocabHashTags = create_vocabulary(it_train_hash)
+vocabHashTags = prune_vocabulary(vocabHashTags, term_count_min = 2)
+vectorizerHashTags = vocab_vectorizer(vocabHashTags)
+dtm_train_hash_tags = create_dtm(it_train_hash, vectorizerHashTags)
+
+#Concatenar resultados
+dataFrameTexto <- as.data.frame(as.matrix(dtm_train_texto))
+dataFrameHash <- as.data.frame(as.matrix(dtm_train_hash_tags))
+
+maFinal <- cbind.fill(subset(dados, select = -c(textParser, id, hashtags, textOriginal, entidades, enriquecimentoTypes, types)), dataFrameTexto)
+maFinal <- cbind.fill(maFinal, dataFrameHash)
+save(maFinal, file = "rdas/2gram-entidades-hora-erro-sem-entidades.Rda")
--- svm/spikes/classifier.R
+++ svm/spikes/classifier.R
@@ -65,16 +65,30 @@ split=0.80
 # 	save.image(file=fileName)
 # })
 
+# try({
+#   load(file = "rdas/2gram-entidades-hora-erro-sem-controle.Rda")
+#   maFinal$resposta <- as.factor(maFinal$resposta)
+#   trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+#   data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+#   data_test <- maFinal[-trainIndex,]
+
+#   treegram25NotNullSemControle <- treinar(data_train)
+#   treegram25NotNullSemControle
+#   matriz3Gram25NotNullSemControle <- getMatriz(treegram25NotNullSemControle, data_test)
+#   resultados <- addRow(resultados, "2 GRAM - Entidades - Erro - Sem Controle", matriz3Gram25NotNullSemControle)
+#   save.image(file=fileName)
+# })
+
 try({
-	load(file = "rdas/2gram-entidades-hora-erro-sem-controle.Rda")
+	load(file = "rdas/2gram-entidades-hora-erro-sem-entidades.Rda")
 	maFinal$resposta <- as.factor(maFinal$resposta)
 	trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
 	data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
 	data_test <- maFinal[-trainIndex,]
 
-	treegram25NotNullSemControle <- treinar(data_train)
-	treegram25NotNullSemControle
-	matriz3Gram25NotNullSemControle <- getMatriz(treegram25NotNullSemControle, data_test)
-	resultados <- addRow(resultados, "2 GRAM - Entidades - Erro - Sem Controle", matriz3Gram25NotNullSemControle)
+	treegram25NotNullSemEntidades <- treinar(data_train)
+	treegram25NotNullSemEntidades
+	matriz3Gram25NotNullSemEntidades <- getMatriz(treegram25NotNullSemEntidades, data_test)
+	resultados <- addRow(resultados, "2 GRAM - Entidades - Erro - Sem Entidades", matriz3Gram25NotNullSemEntidades)
 	save.image(file=fileName)
 })
