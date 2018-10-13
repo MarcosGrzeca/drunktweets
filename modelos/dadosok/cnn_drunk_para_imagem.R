4640b7c120811f52b629fcad5cb26917de4a1471
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-10-12 21:04:45 -0300

    Embeddings

--- modelos/dadosok/cnn_drunk_para_imagem.R
+++ modelos/dadosok/cnn_drunk_para_imagem.R
@@ -13,17 +13,6 @@ source(file_path_as_absolute("utils/tokenizer.R"))
 
 load("rdas/sequences.RData")
 
-FLAGS <- flags(
-  flag_numeric("dropout1", 0.2),
-  flag_numeric("dropout2", 0.2),
-
-  flag_integer("dense_units1", 128),
-  flag_integer("dense_units2", 128),
-
-  flag_integer("epochs", 4),
-  flag_integer("batch_size", 64)
-)
-
 # Data Preparation --------------------------------------------------------
 # Parameters --------------------------------------------------------------
 embedding_dims <- 100
@@ -34,26 +23,24 @@ hidden_dims <- 200
 main_input <- layer_input(shape = c(maxlen), dtype = "int32")
 ccn_out <- main_input %>% 
   layer_embedding(vocab_size, embedding_dims, input_length = maxlen) %>%
-  layer_dropout(FLAGS$dropout1) %>%
   layer_conv_1d(
     filters, kernel_size, 
     padding = "valid", activation = "relu", strides = 1
   ) %>%
   layer_global_max_pooling_1d() %>%
   layer_dense(hidden_dims) %>%
-  layer_dropout(FLAGS$dropout2) %>%
   layer_activation("relu")
 
 auxiliary_input <- layer_input(shape = c(max_sequence))
 entities_out <- auxiliary_input %>%  
-                layer_dense(units = FLAGS$dense_units1, activation = 'relu')
+  layer_dense(units = 10, activation = 'relu')
 
 auxiliary_input_types <- layer_input(shape = c(max_sequence_types))
 types_out <- auxiliary_input_types %>%  
-                layer_dense(units = FLAGS$dense_units1, activation = 'relu')
+  layer_dense(units = 10, activation = 'relu')
 
 main_output <- layer_concatenate(c(ccn_out, entities_out, types_out)) %>%  
-  layer_dense(units = FLAGS$dense_units2, activation = 'relu') %>% 
+  layer_dense(units = 10, activation = 'relu') %>% 
   layer_dense(units = 1, activation = 'sigmoid')
 
 model <- keras_model(
@@ -70,7 +57,7 @@ model %>% compile(
 
 callbacks = list(
   callback_tensorboard(
-    log_dir = "tensorruns/c",
+    log_dir = "tensorruns/new2",
     histogram_freq = 1,
     embeddings_freq = 1
   )
@@ -80,8 +67,8 @@ history <- model %>%
   fit(
     x = list(train_vec$new_textParser, sequences, sequences_types),
     y = array(dados_train$resposta),
-    batch_size = FLAGS$batch_size,
-    epochs = FLAGS$epochs,
+    batch_size = 64,
+    epochs = 3,
     validation_split = 0.2,
     callbacks = callbacks
   )
