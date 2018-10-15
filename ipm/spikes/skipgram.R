889746775b15be11f6895300df230a273a392ca0
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-10-14 21:47:42 -0300

    Testes IPM

--- ipm/spikes/skipgram.R
+++ ipm/spikes/skipgram.R
@@ -16,9 +16,7 @@ CORES <- 5
 registerDoMC(CORES)
 
 word_count <- str_count(dadosTreinarEmbeddings$textEmbedding, "\\S+" )
-reviews <- dadosTreinarEmbeddings$textEmbedding[word_count > 2]
-str(dadosTreinarEmbeddings$textEmbedding)
-str(reviews)
+reviews <- dadosTreinarEmbeddings$textEmbedding[word_count > 4]
 
 library(keras)
 tokenizer <- text_tokenizer(num_words = 40000)
@@ -35,16 +33,17 @@ library(purrr)
 skipgrams_generator <- function(text, tokenizer, window_size, negative_samples) {
   gen <- texts_to_sequences_generator(tokenizer, sample(text))
   function() {
-    skip <- iter_next(gen) %>%
-      skipgrams(
-        vocabulary_size = tokenizer$num_words, 
-        #vocabulary_size = length(tokenizer$word_index), 
-        window_size = window_size, 
-        negative_samples = 1
-      )
-    x <- transpose(skip$couples) %>% map(. %>% unlist %>% as.matrix(ncol = 1))
-    y <- skip$labels %>% as.matrix(ncol = 1)
-    list(x, y)
+    try({
+      skip <- iter_next(gen) %>%
+        skipgrams(
+          vocabulary_size = tokenizer$num_words, 
+          window_size = window_size, 
+          negative_samples = 1
+        )
+      x <- transpose(skip$couples) %>% map(. %>% unlist %>% as.matrix(ncol = 1))
+      y <- skip$labels %>% as.matrix(ncol = 1)
+      list(x, y)
+    })
   }
 }
 
