f1664ed10b022722ede9dcb9a948616afddaa068
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-10-20 16:33:00 -0300

    Exp 3 glove

--- baseline/dados.R
+++ baseline/dados.R
@@ -9,7 +9,7 @@ DATABASE <- "icwsm"
 
 getDadosBaseline <- function() {
       dados <- query("SELECT id,
-                      q2 as resposta,
+                      q3 as resposta,
                       textoParserRisadaEmoticom,
                       textEmbedding,
                       hashtags,
@@ -46,8 +46,8 @@ getDadosBaseline <- function() {
                       FROM tweets t
                       WHERE LENGTH(textoParserRisadaEmoticom) > 5
                       -- AND q3 IS NOT NULL
-                      AND q2 IS NOT NULL
-                      AND q1 = 1
+                      AND q3 IS NOT NULL
+                      AND q2 = 1
                       ")
 
   #AND palavra IN ('/food and drink/beverages/alcoholic beverages/cocktails and beer','/food and drink/beverages/alcoholic beverages/wine','beer','/food and drink','Alcoholic beverage','wine','Beer','/science/chemistry','/health and fitness/disease/cold and flu','/shopping/gifts/party supplies','party','/art and entertainment/movies and tv/movies','alcohol','/health and fitness/addiction','/law, govt and politics/law enforcement/police','/health and fitness/addiction/alcoholism','Wine','Music video','SHOT','NEW MUSIC VIDEO','J.CLANCY','shot','Gentlemen\'s club','LIFE','/sports/golf','/food and drink/beverages','/society/sex','/sports/tennis','Party','club','Dinosaur Bar-B-Que','Public house','/art and entertainment/music','/sports/polo','/health and fitness/addiction/substance abuse','tequila','/travel/specialty travel/vineyards','Ale','vodka','/sports/basketball','/food and drink/cuisines/mexican cuisine','/art and entertainment/movies and tv/comedies','/art and entertainment/dance','Ethanol','Beer pong','/pets/reptiles','England','liquor','Club')
--- ipm/glove/exp3_glove.R
+++ ipm/glove/exp3_glove.R
@@ -1,148 +1,18 @@
 library(tools)
 
-fileName <- "ipm/results_q3_glove.Rdata"
+fileName <- "ipm/results_q1_glove.Rdata"
 source(file_path_as_absolute("ipm/loads.R"))
 
-DESC <- "Exp3 GloVe- CNN + Semantic Enrichment + Word embeddings"
+DESC <- "Exp1 GloVe- CNN + Semantic Enrichment + Word embeddings"
 
-#Geração dos dados
-glove_dir = "C:/wamp64/www/drunktweets/glove/glove.twitter.27B"
-
-lines <- readLines(file.path(glove_dir, "glove.twitter.27B.100d.txt"))
-embeddings_index <- new.env(hash = TRUE, parent = emptyenv())
-for (i in 1:length(lines)) {
-  line <- lines[[i]]
-  values <- strsplit(line, " ")[[1]]
-  word <- values[[1]]
-  embeddings_index[[word]] <- as.double(values[-1])
-}
-cat("Found", length(embeddings_index), "word vectors.\n")
+source(file_path_as_absolute("ipm/glove/load.R"))
 
 #Section: Dados classificar
 dados <- getDadosBaseline()
 
-#Preparação dos dados
-maxlen <- 38
-max_words <- 5000
-tokenizer <-  text_tokenizer(num_words = max_words) %>%
-              fit_text_tokenizer(dados$textEmbedding)
-
-sequences <- texts_to_sequences(tokenizer, dados$textEmbedding)
-word_index = tokenizer$word_index
-
-vocab_size <- length(word_index)
-vocab_size <- vocab_size + 1
-
-cat("Found", length(word_index), "unique tokens.\n")
-data <- pad_sequences(sequences, maxlen = maxlen)
-
-tokenizer_entities <- text_tokenizer(num_words = 1000) %>%
-  fit_text_tokenizer(dados$entidades)
-
-vocabEntitiesLenght <- length(tokenizer_entities$word_index)
-dados$sequences <- texts_to_sequences(tokenizer_entities, dados$entidades)
-
-tokenizer_types <- text_tokenizer(num_words = 1000) %>%
-  fit_text_tokenizer(dados$types)
-vocabTypesLenght <- length(tokenizer_types$word_index)
-dados$sequences_types <- texts_to_sequences(tokenizer_types, dados$types)
-
-library(caret)
-trainIndex <- createDataPartition(dados$resposta, p=0.8, list=FALSE)
-dados_train <- dados[ trainIndex,]
-dados_test <- dados[-trainIndex,]
-
-dados_train_sequence <- data[ trainIndex,]
-dados_test_sequence <- data[-trainIndex,]
-
-max_sequence <- max(sapply(dados_train$sequences, max))
-max_sequence_types <- max(sapply(dados_train$sequences_types, max))
-
-train_sequences <- vectorize_sequences(dados_train$sequences, dimension = max_sequence)
-train_sequences_types <- vectorize_sequences(dados_train$sequences_types, dimension = max_sequence_types)
-
-test_sequences <- vectorize_sequences(dados_test$sequences, dimension = max_sequence)
-test_sequences_types <- vectorize_sequences(dados_test$sequences_types, dimension = max_sequence_types)
-
-max_words <- vocab_size
-word_index <- tokenizer$word_index
-embedding_dims <- 100
-embedding_matrix <- array(0, c(max_words, embedding_dims))
-
-for (word in names(word_index)) {
-  index <- word_index[[word]]
-  if (index < max_words) {
-    embedding_vector <- embeddings_index[[word]]
-    if (!is.null(embedding_vector))
-      embedding_matrix[index+1,] <- embedding_vector
-  }
-}
-
-for (year in 1:10){
-  try({
-		# Parameters --------------------------------------------------------------
-		filters <- 200
-		kernel_size <- 10
-		hidden_dims <- 200
-		
-		main_input <- layer_input(shape = c(maxlen), dtype = "int32")
-		relu <- main_input %>% 
-		  layer_embedding(vocab_size, embedding_dims, input_length = maxlen) %>%
-		  layer_dropout(0.1) %>%
-		  layer_conv_1d(
-		    filters, kernel_size,
-		    padding = "valid", activation = "relu", strides = 1
-		  ) %>%
-		  layer_global_max_pooling_1d() %>%
-		  layer_dense(hidden_dims)
-
-		auxiliary_input <- layer_input(shape = c(max_sequence))
-		entities_out <- auxiliary_input
-
-		auxiliary_input_types <- layer_input(shape = c(max_sequence_types))
-		types_out <- auxiliary_input_types
-
-		main_output <- layer_concatenate(c(relu, entities_out, types_out)) %>%  
-		  layer_dense(units = 64, activation = 'relu') %>% 
-		  layer_dropout(0.2) %>%
-		  layer_dense(units = 32, activation = "relu") %>%
-		  layer_dense(units = 1, activation = 'sigmoid')
-
-		model <- keras_model(
-		  inputs = c(main_input, auxiliary_input, auxiliary_input_types),
-		  outputs = main_output
-		)
-		
-		get_layer(model, index = 1) %>%
-		  set_weights(list(embedding_matrix)) %>%
-		  freeze_weights()
-
-		model %>% compile(
-		  loss = "binary_crossentropy",
-		  optimizer = "adam",
-		  metrics = "accuracy"
-		)
-
-		library(keras)
-		# Training ----------------------------------------------------------------        
-		history <- model %>%
-		  fit(
-		    x = list(dados_train_sequence, train_sequences, train_sequences_types),
-		    y = array(dados_train$resposta),
-		    batch_size = 64,
-		    epochs = 3,
-		    validation_split = 0.2
-		  )
-
-		# history
-		predictions <- model %>% predict(list(dados_test_sequence, test_sequences, test_sequences_types))
-		predictions2 <- round(predictions, 0)
-
-		matriz <- confusionMatrix(data = as.factor(predictions2), as.factor(dados_test$resposta), positive="1")
-		resultados <- addRowAdpater(resultados, DESC, matriz)
-  })
-}
-#save.image(file=fileName)
+try({
+	source(file_path_as_absolute("ipm/glove/ipmtrain_embedding.R"))
+	})
 resultados$F1
 resultados$Precision
 resultados$Recall
