c7157843e0149626d1a0cd552f1099d792b282ad
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-10-20 22:34:25 -0300

    mais um teste

---
+++ ipm/skipgram/exp1_skipgram.R
@@ -0,0 +1,20 @@
+library(tools)
+
+fileName <- "ipm/results_q1_glove.Rdata"
+source(file_path_as_absolute("ipm/loads.R"))
+
+DESC <- "Exp1 SkipGram- CNN + Semantic Enrichment + Word embeddings"
+
+load(file = "ipm/embeddings/skipgram_glove.Rda")
+
+#Section: Dados classificar
+dados <- getDadosBaseline()
+
+try({
+	maxlen <- 38
+	max_words <- 7452
+	source(file_path_as_absolute("ipm/skipgram/ipmtrain_embedding.R"))
+})
+resultados$F1
+resultados$Precision
+resultados$Recall
