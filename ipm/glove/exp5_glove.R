f15f42c9e82d479de47dc94b452dfc2898bcf437
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-11-12 18:58:49 -0200

    Bora

---
+++ ipm/glove/exp5_glove.R
@@ -0,0 +1,21 @@
+library(tools)
+
+fileName <- "ipm/results_q1_glove.Rdata"
+source(file_path_as_absolute("ipm/loads.R"))
+
+DESC <- "Exp1 GloVe- CNN + Semantic Enrichment + Word embeddings"
+
+source(file_path_as_absolute("ipm/glove/load.R"))
+source(file_path_as_absolute("utils/getDados.R"))
+
+#Section: Dados classificar
+dados <- getDadosChat()
+
+try({
+	maxlen <- 50
+	max_words <- 40000
+	source(file_path_as_absolute("ipm/glove/ipmtrain_embedding.R"))
+	})
+resultados$F1
+resultados$Precision
+resultados$Recall
