57ccffa10cf36f16b1b7a094afac484311ed548d
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-11-12 17:29:36 -0200

    bora

---
+++ chat/gerar_data/generate_sequence.R
@@ -0,0 +1,75 @@
+library(keras)
+library(readr)
+library(stringr)
+library(purrr)
+library(tibble)
+library(dplyr)
+library(tools)
+
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+dados <- getDadosChat()
+
+tokenizer <- text_tokenizer(num_words = 1000) %>%
+             fit_text_tokenizer(dados$entidades)
+vocabEntitiesLenght <- length(tokenizer$word_index)
+dados$sequences <- texts_to_sequences(tokenizer, dados$entidades)
+
+tokenizer_types <- text_tokenizer(num_words = 1000) %>%
+             fit_text_tokenizer(dados$types)
+vocabTypesLenght <- length(tokenizer_types$word_index)
+dados$sequences_types <- texts_to_sequences(tokenizer_types, dados$types)
+
+library(doMC)
+library(mlbench)
+
+CORES <- 4
+registerDoMC(CORES)
+
+#Separação teste e treinamento
+set.seed(10)
+split=0.80
+
+trainIndex <- createDataPartition(dados$resposta, p=split, list=FALSE)
+
+dados_train <- dados[ trainIndex,]
+dados_test <- dados[-trainIndex,]
+
+# Texto
+dadosTransformado <- dados_train %>%
+  mutate(
+    textEmbedding = map(textEmbedding, ~tokenize_words(.x))
+  ) %>%
+  select(textEmbedding)
+
+dadosTransformadoTest <- dados_test %>%
+  mutate(
+    textEmbedding = map(textEmbedding, ~tokenize_words(.x))
+  ) %>%
+  select(textEmbedding)
+
+all_data <- bind_rows(dadosTransformado, dadosTransformadoTest)
+
+#Vocabulario texto
+vocab <- c(unlist(dadosTransformado$textEmbedding), unlist(dadosTransformadoTest$textEmbedding)) %>%
+  unique() %>%
+  sort()
+
+vocab_size <- length(vocab) + 1
+maxlen <- map_int(all_data$textEmbedding, ~length(.x)) %>% max()
+
+train_vec <- vectorize_stories(dadosTransformado, vocab, maxlen)
+
+#Vocabulario enttidades
+max_sequence <- max(sapply(dados_train$sequences, max))
+sequences <- vectorize_sequences(dados_train$sequences, dimension = max_sequence)
+
+max_sequence_types <- max(sapply(dados_train$sequences_types, max))
+sequences_types <- vectorize_sequences(dados_train$sequences_types, dimension = max_sequence_types)
+
+#Generate Test
+test_vec <- vectorize_stories(dadosTransformadoTest, vocab, maxlen)
+sequences_test <- vectorize_sequences(dados_test$sequences, dimension = max_sequence)
+sequences_test_types <- vectorize_sequences(dados_test$sequences_types, dimension = max_sequence_types)
+
+save.image(file="chat/rdas/sequencesexp4.RData")
