34b852223be239f186158b9f8db559acdd18a8fb
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-11-12 20:45:50 -0200

    Bora treinar

---
+++ ipm/skipgram/exp5_skipgram.R
@@ -0,0 +1,21 @@
+library(tools)
+
+fileName <- "ipm/results_q1_glove.Rdata"
+source(file_path_as_absolute("ipm/loads.R"))
+
+DESC <- "Exp5 SkipGram- CNN + Semantic Enrichment + Word embeddings"
+
+load(file = "ipm/embeddings/skipgram_glove.Rda")
+
+#Section: Dados classificar
+dados <- getDadosChat()
+
+try({
+	maxlen <- 38
+	max_words <- 40000
+	epochs_num <- 3
+	source(file_path_as_absolute("ipm/skipgram/ipmtrain_embedding.R"))
+})
+resultados$F1
+resultados$Precision
+resultados$Recall
