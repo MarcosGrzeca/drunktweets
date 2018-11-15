ff7e2bf9af682b2cedcdc68000ad05284000cbf0
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-11-15 14:45:50 -0200

    Ensemble exp 2

---
+++ ensemble/run/exp2.R
@@ -0,0 +1,12 @@
+library(tools)
+
+imageFile <- "ensemble/resultados/exp2/imageFile.RData"
+
+datasetFile <-"ensemble/datasets/exp2/2-Gram-dbpedia-types-enriquecimento-info-q2-not-null_info_entidades.Rda"
+
+baseResultsFiles <- "ensemble/resultados/exp2/"
+baseResampleFiles <- "ensemble/resample/exp2/"
+
+fileResults <- "ensemble/resultados/exp2/"
+
+source(file_path_as_absolute("ensemble/run/core.R"))
