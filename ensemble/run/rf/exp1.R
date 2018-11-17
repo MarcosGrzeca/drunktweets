911a9ddaebd1f856399e351549c80cf0e368ed15
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-11-17 16:20:34 -0200

    Ensemble RF

---
+++ ensemble/run/rf/exp1.R
@@ -0,0 +1,12 @@
+library(tools)
+
+imageFile <- "ensemble/resultados/exp1/imageFileRF.RData"
+
+datasetFile <-"ensemble/datasets/exp1/2-Gram-dbpedia-types-enriquecimento-info-q1-not-null_info_entidades.Rda"
+
+baseResultsFiles <- "ensemble/resultados/exp1/"
+baseResampleFiles <- "ensemble/resample/exp1/"
+
+fileResults <- "ensemble/resultados/exp1/"
+
+source(file_path_as_absolute("ensemble/run/rf/core.R"))
