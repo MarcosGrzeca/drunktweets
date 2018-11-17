475f8738a1d03401315d1335164b29a80a0a8316
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-11-17 16:45:24 -0200

    Exp 4 ensemble

---
+++ ensemble/run/networkglove/exp4.R
@@ -0,0 +1,26 @@
+library(tools)
+
+imageFile <- "ensemble/resultados/exp4/imageFileRede.RData"
+
+baseResultsFiles <- "ensemble/resultados/exp4/"
+baseResampleFiles <- "ensemble/resample/exp4/"
+
+fileResults <- "ensemble/resultados/exp4/"
+
+source(file_path_as_absolute("ipm/experimenters.R"))
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("baseline/dados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+source(file_path_as_absolute("ipm/glove/load.R"))
+
+#Section: Dados classificar
+dados <- getDadosInfoGain()
+
+try({
+	maxlen <- 50
+	max_words <- 40000
+	source(file_path_as_absolute("ensemble/run/networkglove/ipmtrain_embedding.R"))
+})
+resultados$F1
+resultados$Precision
+resultados$Recall
