bf5f03bc7b2b78bbd98f85273a43c0148c035ac6
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-11-23 23:37:15 -0200

    Ensemble NB

---
+++ ensemble/run/nb/exp3.R
@@ -0,0 +1,11 @@
+library(tools)
+
+imageFile <- "ensemble/resultados/exp3/imageFileNB.RData"
+datasetFile <-"ensemble/datasets/exp3/2-Gram-dbpedia-types-enriquecimento-info-q3-not-null_info_entidades.Rda"
+
+baseResultsFiles <- "ensemble/resultados/exp3/"
+baseResampleFiles <- "ensemble/resample/exp3/"
+
+fileResults <- "ensemble/resultados/exp3/"
+
+source(file_path_as_absolute("ensemble/run/nb/core.R"))
