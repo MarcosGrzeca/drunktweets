b16aa000d3d96226f2977235292e124751b6f156
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-12-11 20:19:12 -0200

    RF exp 5

---
+++ ensemble/run/rf/exp5.R
@@ -0,0 +1,11 @@
+library(tools)
+
+imageFile <- "ensemble/resultados/exp5/imageFileRF.RData"
+datasetFile <-"chat/rdas/2gram-entidades-erro.Rda"
+
+baseResultsFiles <- "ensemble/resultados/exp5/"
+baseResampleFiles <- "ensemble/resample/exp5/"
+
+fileResults <- "ensemble/resultados/exp5/"
+
+source(file_path_as_absolute("ensemble/run/rf/core.R"))
