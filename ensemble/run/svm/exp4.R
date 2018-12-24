21a3392cd744daf909d50574c8777b19636014ba
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-12-24 15:54:28 -0200

    EXP4 svm

--- ensemble/run/svm/exp4.R
+++ ensemble/run/svm/exp4.R
@@ -2,7 +2,8 @@ library(tools)
 
 imageFile <- "ensemble/resultados/exp4/imageFile.RData"
 
-datasetFile <-"rdas/2gram-entidades-hora-erro.Rda"
+# datasetFile <-"rdas/2gram-entidades-hora-erro.Rda"
+datasetFile <-"rdas/2gram-entidades-hora-erro-semkeywords.Rda"
 
 baseResultsFiles <- "ensemble/resultados/exp4/"
 baseResampleFiles <- "ensemble/resample/exp4/"
