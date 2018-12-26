ebcf73d2befd2ad7e393a1a14a0897ba862b6667
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-12-26 20:43:31 -0200

    RF exp4

---
+++ ensemble/run/rf/exp4.R
@@ -0,0 +1,11 @@
+library(tools)
+
+imageFile <- "ensemble/resultados/exp4/imageFileRF.RData"
+datasetFile <-"rdas/2gram-entidades-hora-erro-semkeywords.Rda"
+
+baseResultsFiles <- "ensemble/resultados/exp4/"
+baseResampleFiles <- "ensemble/resample/exp4/"
+
+fileResults <- "ensemble/resultados/exp4/"
+
+source(file_path_as_absolute("ensemble/run/rf/core.R"))
