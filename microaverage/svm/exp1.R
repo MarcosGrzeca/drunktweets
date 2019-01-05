1e7b7eea183afaedf86abab952a9db1b192797eb
Marcos Grzeca <marcos@tiny.com.br>
2019-01-05 17:31:40 -0200

    Experimentos microaverage

---
+++ microaverage/svm/exp1.R
@@ -0,0 +1,8 @@
+library(tools)
+
+# datasetFile <-"ensemble/datasets/exp2/2-Gram-dbpedia-types-enriquecimento-info-q2-not-null_info_entidades.Rda"
+datasetFile <-"ensemble/datasets/exp1/2-Gram-dbpedia-types-enriquecimento-info-q1-not-null_info_entidades.Rda"
+fileExpName <- "svm"
+expName <- "exp1"
+
+source(file_path_as_absolute("microaverage/core.R"))
