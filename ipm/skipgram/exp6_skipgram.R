a0f56338c28b75e4e1fde504e65a910857785fce
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-02-23 23:32:10 -0300

    Skipgram generator exp6

---
+++ ipm/skipgram/exp6_skipgram.R
@@ -0,0 +1,21 @@
+library(tools)
+
+fileName <- "ipm/results_exp6_glove.Rdata"
+source(file_path_as_absolute("ipm/loads.R"))
+
+DESC <- "Exp6 SkipGram- CNN + Semantic Enrichment + Word embeddings"
+
+load(file = "ipm/embeddings/skipgram_glove.Rda")
+
+#Section: Dados classificar
+dados <- getDadosAmazon()
+
+try({
+	maxlen <- 45
+	max_words <- 10000
+	epochs_num <- 3
+	source(file_path_as_absolute("ipm/skipgram/ipmtrain_embedding.R"))
+})
+resultados$F1
+resultados$Precision
+resultados$Recall
