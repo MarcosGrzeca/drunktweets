27fb0c087be32d1bf4cb3bda3c5da5abd65a0fc4
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-03-03 11:33:13 -0300

    Zole testar

--- ipm/expsperdidos/exp5enriquecimento.R
+++ ipm/expsperdidos/exp5enriquecimento.R
@@ -11,25 +11,24 @@ for (epoch in epochs) {
 		  try({
 		  	enriquecimento <- 1
 		  	load("chat/rdas/sequencesexp5-semkw.RData")
+		    
 		    # Data Preparation --------------------------------------------------------
 			# Parameters --------------------------------------------------------------
 			embedding_dims <- 100
-
-			# Parameters --------------------------------------------------------------
 			filters <- 200
 			kernel_size <- 10
 			hidden_dims <- 200
 
 			main_input <- layer_input(shape = c(maxlen), dtype = "int32")
-			relu <- main_input %>% 
-			  layer_embedding(vocab_size, embedding_dims, input_length = maxlen) %>%
-			  layer_dropout(0.1) %>%
-			  layer_conv_1d(
-			    filters, kernel_size,
-			    padding = "valid", activation = "relu", strides = 1
-			  ) %>%
-			  layer_global_max_pooling_1d() %>%
-			  layer_dense(hidden_dims)
+			ccn_out <- main_input %>% 
+				layer_embedding(vocab_size, embedding_dims, input_length = maxlen) %>%
+				layer_dropout(0.1) %>%
+				layer_conv_1d(
+				  filters, kernel_size, 
+				  padding = "valid", activation = "relu", strides = 1
+				) %>%
+				layer_global_max_pooling_1d() %>%
+				layer_dense(hidden_dims)
 
 			auxiliary_input <- layer_input(shape = c(max_sequence))
 			entities_out <- auxiliary_input
@@ -37,69 +36,34 @@ for (epoch in epochs) {
 			auxiliary_input_types <- layer_input(shape = c(max_sequence_types))
 			types_out <- auxiliary_input_types
 
+			main_output <- layer_concatenate(c(ccn_out, entities_out, types_out)) %>%  
+				layer_dense(units = 64, activation = 'relu') %>% 
+				layer_dropout(0.2) %>%
+				layer_dense(units = 32, activation = "relu") %>%
+				layer_dense(units = 1, activation = 'sigmoid')
 
-			if (enriquecimento == 1) {
-			  main_output <- layer_concatenate(c(relu, entities_out, types_out)) %>%  
-			    layer_dense(units = 64, activation = 'relu') %>% 
-			    layer_dropout(0.2) %>%
-			    layer_dense(units = 32, activation = "relu") %>%
-			    layer_dense(units = 1, activation = 'sigmoid')
+			model <- keras_model(
+				inputs = c(main_input, auxiliary_input, auxiliary_input_types),
+				outputs = main_output
+			)
 
-			    model <- keras_model(
-			    inputs = c(main_input, auxiliary_input, auxiliary_input_types),
-			    outputs = main_output
-			  )
+			# Compile model
+			model %>% compile(
+				loss = "binary_crossentropy",
+				optimizer = "adam",
+				metrics = "accuracy"
+			)
 
-			  model %>% compile(
-			    loss = "binary_crossentropy",
-			    optimizer = "adam",
-			    metrics = "accuracy"
-			  )
+			history <- model %>%
+				fit(
+				  x = list(train_vec$new_textParser, sequences, sequences_types),
+				  y = array(dados_train$resposta),
+				  batch_size = FLAGS$batch_size,
+				  epochs = FLAGS$epochs,
+				  validation_split = 0.2
+				)
 
-			  # Training ----------------------------------------------------------------
-			  history <- model %>%
-			    fit(
-			      x = list(train_vec$textEmbedding, train_sequences, train_sequences_types),
-			      y = array(dados_train$resposta),
-			      batch_size = batch,
-			      epochs = epoch,
-			      validation_split = 0.2
-			    )
-
-			  # history
-			  predictions <- model %>% predict(list(test_vec$textEmbedding, test_sequences, test_sequences_types))
-			} else {
-			  main_output <- relu %>%  
-			    layer_dense(units = 64, activation = 'relu') %>% 
-			    layer_dropout(0.2) %>%
-			    layer_dense(units = 32, activation = "relu") %>%
-			    layer_dense(units = 1, activation = 'sigmoid')
-
-			    model <- keras_model(
-			    inputs = c(main_input),
-			    outputs = main_output
-			  )
-
-			  model %>% compile(
-			    loss = "binary_crossentropy",
-			    optimizer = "adam",
-			    metrics = "accuracy"
-			  )
-
-			  # Training ----------------------------------------------------------------
-			  history <- model %>%
-			    fit(
-			      x = list(train_vec$textEmbedding),
-			      y = array(dados_train$resposta),
-			      batch_size = batch,
-			      epochs = epoch,
-			      validation_split = 0.2
-			    )
-
-			  # history
-			  predictions <- model %>% predict(list(test_vec$textEmbedding))
-			}
-
+			predictions <- model %>% predict(list(test_vec$new_textParser, sequences_test, sequences_test_types))
 			predictions2 <- round(predictions, 0)
 			matriz <- confusionMatrix(data = as.factor(predictions2), as.factor(dados_test$resposta), positive="1")
 			resultados <- addRowAdpater(resultados, paste0("Exp 5 - Hidden - Enr: ", enriquecimento, " Ep: ", epoch, " - Ba: ", " - ", batch), matriz)
