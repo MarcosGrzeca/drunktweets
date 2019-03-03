6f53f830fab387392c2e8fcf41a99e4b8ca54726
root <root@ip-172-31-5-75.us-east-2.compute.internal>
2019-03-03 14:40:03 +0000

    Teste

--- ipm/expsperdidos/exp5enriquecimento.R
+++ ipm/expsperdidos/exp5enriquecimento.R
@@ -67,7 +67,7 @@ for (epoch in epochs) {
 			predictions2 <- round(predictions, 0)
 			matriz <- confusionMatrix(data = as.factor(predictions2), as.factor(dados_test$resposta), positive="1")
 			resultados <- addRowAdpater(resultados, paste0("Exp 5 - Hidden - Enr: ", enriquecimento, " Ep: ", epoch, " - Ba: ", " - ", batch), matriz)
-			saveRDS(resultados, file = "ipm/expsperdidos/resultadosautomatico/resultados_exp5_enriquecidos_1.rds")
+			saveRDS(resultados, file = "ipm/expsperdidos/resultadosautomatico/resultados_exp5_enriquecidos_2.rds")
 		  })
 		}
 	}
