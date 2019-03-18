1b62ccc3ec83b59f10682b6fcde87ecda419c5c0
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-03-18 20:48:30 -0300

    Inclusao dos tipos das redes

--- experimentos/tester_cnn_glove.R
+++ experimentos/tester_cnn_glove.R
@@ -20,7 +20,7 @@ for (file in files) {
 				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
 				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
 				source(file_path_as_absolute(file))
-				logar("DS3", "GloVe", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc)
+				logar("DS3", "GloVe", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc)
 			}
 		}
 	}
