600ca4f9d58dc7be1d553d974c031d34fa46fc40
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-03-18 21:23:21 -0300

    LSTM testes

---
+++ experimentos/tester_lstm.R
@@ -0,0 +1,25 @@
+library(tools)
+source(file_path_as_absolute("ipm/loads.R"))
+
+epocas <- c(3,5,10,20)
+enriquecimentos <- c(0, 1)
+metricas <- c("acc", "val_loss")
+early_stop <- 1
+
+filesFeitos <- c()
+
+files <- c("experimentos/lstm/teste1.R")
+
+for (file in files) {
+	redeDesc <- generateHash(1)
+	for (epoca in epocas) {
+		for (metrica in metricas) {
+			for (enriquecimento in enriquecimentos) {
+				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
+				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
+				source(file_path_as_absolute(file))
+				logar("DS3", "Hidden", "LSTM", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc)
+			}
+		}
+	}
+}
