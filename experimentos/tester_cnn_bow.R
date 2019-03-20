54cf6dbfc6beaf1481d62e0cbe00fa941ad9ae09
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-03-20 16:03:19 -0300

    Zole testar

---
+++ experimentos/tester_cnn_bow.R
@@ -0,0 +1,26 @@
+library(tools)
+source(file_path_as_absolute("ipm/loads.R"))
+
+epocas <- c(3,5,10)
+enriquecimentos <- c(0, 1)
+metricas <- c("acc", "val_loss")
+early_stop <- 1
+
+files <- c("experimentos/rede_bow.R")
+
+for (file in files) {
+	redeDesc <- generateHash(1)
+	redeDesc <- "BoW"
+	for (epoca in epocas) {
+		for (metrica in metricas) {
+			for (enriquecimento in enriquecimentos) {
+				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
+				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
+				source(file_path_as_absolute(file))
+				logar("DS3", "Hidden", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc)
+			}
+		}
+	}
+}
+
+#system("init 0")
