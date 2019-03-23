28783e54e6f94df6783cbfd963aa0106a1dc572e
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-03-23 15:07:33 -0300

    Flatten Glove

---
+++ experimentos/flatten_glove.R
@@ -0,0 +1,178 @@
+library(tools)
+source(file_path_as_absolute("utils/getDadosAmazon.R"))
+source(file_path_as_absolute("utils/getDados.R"))
+
+dados <- getDadosAmazon()
+ 
+maxlen <- 40
+max_words <- 15000
+
+tokenizer <-  text_tokenizer(num_words = max_words) %>%
+              fit_text_tokenizer(dados$textEmbedding)
+
+sequences <- texts_to_sequences(tokenizer, dados$textEmbedding)
+word_index = tokenizer$word_index
+
+vocab_size <- length(word_index)
+vocab_size <- vocab_size + 1
+
+cat("Found", length(word_index), "unique tokens.\n")
+data <- pad_sequences(sequences, maxlen = maxlen)
+
+tokenizer_entities <- text_tokenizer(num_words = 1000) %>%
+  fit_text_tokenizer(dados$entidades)
+
+vocabEntitiesLenght <- length(tokenizer_entities$word_index)
+dados$sequences <- texts_to_sequences(tokenizer_entities, dados$entidades)
+
+tokenizer_types <- text_tokenizer(num_words = 1000) %>%
+  fit_text_tokenizer(dados$types)
+vocabTypesLenght <- length(tokenizer_types$word_index)
+dados$sequences_types <- texts_to_sequences(tokenizer_types, dados$types)
+
+library(caret)
+trainIndex <- createDataPartition(dados$resposta, p=0.8, list=FALSE)
+dados_train <- dados[ trainIndex,]
+dados_test <- dados[-trainIndex,]
+
+dados_train_sequence <- data[ trainIndex,]
+dados_test_sequence <- data[-trainIndex,]
+
+max_sequence <- max(sapply(dados_train$sequences, max))
+max_sequence_types <- max(sapply(dados_train$sequences_types, max))
+
+train_sequences <- vectorize_sequences(dados_train$sequences, dimension = max_sequence)
+train_sequences_types <- vectorize_sequences(dados_train$sequences_types, dimension = max_sequence_types)
+
+test_sequences <- vectorize_sequences(dados_test$sequences, dimension = max_sequence)
+test_sequences_types <- vectorize_sequences(dados_test$sequences_types, dimension = max_sequence_types)
+
+max_words <- vocab_size
+word_index <- tokenizer$word_index
+embedding_dims <- 100
+embedding_matrix <- array(0, c(max_words, embedding_dims))
+
+for (word in names(word_index)) {
+  index <- word_index[[word]]
+  if (index < max_words) {
+    embedding_vector <- embeddings_index[[word]]
+    if (!is.null(embedding_vector))
+      embedding_matrix[index+1,] <- embedding_vector
+  }
+}
+
+library(tools)
+
+resultados <- data.frame(matrix(ncol = 4, nrow = 0))
+names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
+
+library(keras)
+for (year in 1:20) {
+	callbacks_list <- list(
+		callback_early_stopping(
+			monitor = metrica,
+			patience = 1
+		),
+		callback_model_checkpoint(
+			filepath = paste0(redeDesc, "", enriquecimento, "", early_stop, "", "test_models.h5"),
+			monitor = "val_loss",
+			save_best_only = TRUE
+		)
+	)
+
+	FLAGS <- flags(
+		flag_integer("epochs", 3),
+		flag_integer("batch_size", 64)
+	)
+
+	# Data Preparation --------------------------------------------------------
+	# Parameters --------------------------------------------------------------
+	embedding_dims <- 100
+	filters <- 20
+	hidden_dims <- 10
+
+	main_input <- layer_input(shape = c(maxlen), dtype = "int32")
+
+	embedding_input <- 	main_input %>% 
+				 		layer_embedding(input_dim = vocab_size, output_dim = embedding_dims, input_length = maxlen)
+
+	auxiliary_input <- layer_input(shape = c(max_sequence))
+	entities_out <- auxiliary_input %>%
+                layer_dense(units = 4, activation = 'relu', name = "ReLuEntidades")
+
+	auxiliary_input_types <- layer_input(shape = c(max_sequence_types))
+	types_out <- auxiliary_input_types %>%
+                layer_dense(units = 4, activation = 'relu', name = "ReLuTypes")
+
+    flatten_out <- 	embedding_input	 %>%
+				layer_flatten() %>%
+				layer_dense(units = 32, activation = "relu")
+
+	if (enriquecimento == 1) {
+		main_output <- layer_concatenate(c(flatten_out, entities_out, types_out)) %>% 
+				layer_dropout(0.2) %>%
+				layer_dense(units = 6, activation = "relu") %>%
+				layer_dense(units = 1, activation = 'sigmoid')
+
+		model <- keras_model(
+			inputs = c(main_input, auxiliary_input, auxiliary_input_types),
+			outputs = main_output
+		)
+	} else {
+		main_output <- lstm_out %>% 
+				layer_dropout(0.2) %>%
+				layer_dense(units = 4, activation = "relu") %>%
+				layer_dense(units = 1, activation = 'sigmoid')
+
+		model <- keras_model(
+			inputs = c(main_input),
+			outputs = main_output
+		)
+	}
+
+	get_layer(model, index = 1) %>%
+      set_weights(list(embedding_matrix)) %>%
+      freeze_weights()
+
+	# Compile model
+	model %>% compile(
+		loss = "binary_crossentropy",
+		optimizer = "adam",
+		metrics = "accuracy"
+	)
+
+	if (enriquecimento == 1) {
+		history <- model %>%
+			fit(
+			  x = list(dados_train_sequence, train_sequences, train_sequences_types),
+			  y = array(dados_train$resposta),
+			  batch_size = FLAGS$batch_size,
+			  epochs = epoca,
+			  callbacks = callbacks_list,
+			  validation_split = 0.2
+			)
+			
+		# predictions <- model %>% predict(test_vec$new_textParser)
+		# predictions <- model %>% predict(list(test_vec$new_textParser, sequences_test, sequences_test_types))
+		predictions <- model %>% predict(list(dados_test_sequence, test_sequences, test_sequences_types))
+	} else {
+		history <- model %>%
+			fit(
+			  # x = list(train_vec$new_textParser, sequences, sequences_types),
+			  x = list(dados_train_sequence),
+			  y = array(dados_train$resposta),
+			  batch_size = FLAGS$batch_size,
+			  epochs = epoca,
+			  callbacks = callbacks_list,
+			  validation_split = 0.2
+			)
+		predictions <- model %>% predict(list(dados_test_sequence))
+	}
+
+	predictions2 <- round(predictions, 0)
+	matriz <- confusionMatrix(data = as.factor(predictions2), as.factor(dados_test$resposta), positive="1")
+	resultados <- addRowAdpater(resultados, paste0("Enriquecimento: ", enriquecimento, " - Early: ", early_stop), matriz)
+}
+resultados$F1
+resultados$Precision
+resultados$Recall
---
+++ experimentos/tester_flatten_glove.R
@@ -0,0 +1,27 @@
+library(tools)
+source(file_path_as_absolute("ipm/loads.R"))
+
+epocas <- c(3,5,10)
+enriquecimentos <- c(0, 1)
+metricas <- c("acc", "val_loss")
+early_stop <- 1
+
+source(file_path_as_absolute("ipm/glove/load.R"))
+
+files <- c("experimentos/flatten_glove.R")
+
+for (file in files) {
+	redeDesc <- "FlattenGloVe"
+	for (epoca in epocas) {
+		for (metrica in metricas) {
+			for (enriquecimento in enriquecimentos) {
+				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
+				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
+				source(file_path_as_absolute(file))
+				logar("DS3", "GloVe", "Flatten", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc)
+			}
+		}
+	}
+}
+
+system("init 0")
