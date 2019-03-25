ec9272c1e26da2daa30227d06e0ce3ce338ccff4
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-03-25 20:21:59 -0300

    bora

--- experimentos/ds1/q3/cnn/cnn3janelas_q3.R
+++ experimentos/ds1/q3/cnn/cnn3janelas_q3.R
@@ -2,4 +2,4 @@
 library("tools")
 
 source(file_path_as_absolute("experimentos/ds1/getDadosQ1.R"))
-source(file_path_as_absolute("experimentos/ds1/cnn/cnnjanelas2.R"))
+source(file_path_as_absolute("experimentos/ds1/cnn/cnnjanelas.R"))
