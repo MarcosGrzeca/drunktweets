6eb915f1a0208c93faccedec640a0575dbfce1b4
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-03-26 22:59:03 -0300

    zole experimentos

--- experimentos/ds1/q2/lstm/glove_bow_q2.R
+++ experimentos/ds1/q2/lstm/glove_bow_q2.R
@@ -3,4 +3,4 @@ library("tools")
 
 bow <- 1
 source(file_path_as_absolute("experimentos/ds1/q2/getDadosQ2.R"))
-source(file_path_as_absolute("experimentos/ds1/lstm/glove_bow.R"))
+source(file_path_as_absolute("experimentos/ds1/cnn/glove_bow.R"))
