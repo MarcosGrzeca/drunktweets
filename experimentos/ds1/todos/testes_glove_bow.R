f2749e48672421e02f306c8d86e053c078a1e3f4
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-03-26 21:46:23 -0300

    testanxdo

--- experimentos/ds1/todos/testes_glove_bow.R
+++ experimentos/ds1/todos/testes_glove_bow.R
@@ -1,65 +1,63 @@
 library(tools)
 source(file_path_as_absolute("ipm/loads.R"))
-
-epocas <- c(3,5,10)
-enriquecimentos <- c(0, 1)
-metricas <- c("acc", "val_loss")
+source(file_path_as_absolute("ipm/glove/load.R"))
 early_stop <- 1
 
-files <- c("experimentos/ds1/q1/lstm/lstm_bow_q1.R")
+epocas <- c(3,5,10,20)
+enriquecimentos <- c(0, 1)
+metricas <- c("acc", "val_loss")
+
+files <- c("experimentos/ds1/q1/cnn/glove_bow_q1.R")
 
 for (file in files) {
-	redeDesc <- "BoWLSTM_DS1Q1"
+	redeDesc <- "CNNGloveBowDS1Q1"
 	for (epoca in epocas) {
 		for (metrica in metricas) {
 			for (enriquecimento in enriquecimentos) {
 				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
 				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
 				source(file_path_as_absolute(file))
-				logar("DS1-Q1", "Hidden", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc, file)
+				logar("DS1-Q1", "GloVe", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc)
 			}
 		}
 	}
 }
 
-epocas <- c(3,5,10)
+epocas <- c(3,5,10,20)
 enriquecimentos <- c(0, 1)
 metricas <- c("acc", "val_loss")
 
-files <- c("experimentos/ds1/q2/lstm/lstm_bow_q2.R")
+files <- c("experimentos/glove_bow.R")
 
 for (file in files) {
-	redeDesc <- "BoWLSTM_DS1Q2"
+	redeDesc <- "CNNGloveBowDS1Q2"
 	for (epoca in epocas) {
 		for (metrica in metricas) {
 			for (enriquecimento in enriquecimentos) {
 				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
 				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
 				source(file_path_as_absolute(file))
-				logar("DS1-Q2", "Hidden", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc, file)
+				logar("DS1-Q2", "GloVe", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc)
 			}
 		}
 	}
 }
 
-system("init 0")
-
-
-epocas <- c(3,5,10)
+epocas <- c(3,5,10,20)
 enriquecimentos <- c(0, 1)
 metricas <- c("acc", "val_loss")
 
-files <- c("experimentos/ds1/q3/lstm/lstm_bow_q3.R")
+files <- c("experimentos/glove_bow.R")
 
 for (file in files) {
-	redeDesc <- "BoWLSTM_DS1Q3"
+	redeDesc <- "CNNGloveBowDS1Q3"
 	for (epoca in epocas) {
 		for (metrica in metricas) {
 			for (enriquecimento in enriquecimentos) {
 				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
 				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
 				source(file_path_as_absolute(file))
-				logar("DS1-Q3", "Hidden", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc, file)
+				logar("DS1-Q3", "GloVe", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc)
 			}
 		}
 	}
