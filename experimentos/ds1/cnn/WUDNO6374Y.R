7a9b66e9dbeeabd2f4a342c90f42cd3b23a1fbcb
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-03-28 10:23:30 -0300

    Novos experimentos

---
+++ experimentos/ds1/cnn/WUDNO6374Y.R
@@ -0,0 +1,110 @@
+library(keras)
+
+for (year in 1:20) {
+	callbacks_list <- list(
+		callback_early_stopping(
+			monitor = metrica,
+			patience = 1
+		),
+		callback_model_checkpoint(
+			filepath = paste0(redeDesc, "", enriquecimento, "", early_stop, "", "test_models.h5"),
+			monitor = "val_loss",
+			save_best_only = TRUE
+		)
+	)
+
+	# Data Preparation --------------------------------------------------------
+	# Parameters --------------------------------------------------------------
+	embedding_dims <- 100
+	filters <- 20
+	hidden_dims <- 10
+
+	main_input <- layer_input(shape = c(maxlen), dtype = "int32")
+
+	embedding_input <- 	main_input %>% 
+				 		layer_embedding(input_dim = vocab_size, output_dim = embedding_dims, input_length = maxlen)
+
+	auxiliary_input <- layer_input(shape = c(max_sequence))
+	entities_out <- auxiliary_input
+
+	auxiliary_input_types <- layer_input(shape = c(max_sequence_types))
+	types_out <- auxiliary_input_types
+
+	ccn_out_3 <- embedding_input %>% 
+		layer_conv_1d(
+			filters, 3,
+		 	padding = "valid", activation = "relu", strides = 1
+		) %>%
+		layer_global_max_pooling_1d()
+
+	ccn_out_4 <- embedding_input %>% 
+		layer_conv_1d(
+		  filters, 4, 
+		  padding = "valid", activation = "relu", strides = 1
+		) %>%
+		layer_global_max_pooling_1d()
+
+	ccn_out_5 <- embedding_input %>% 
+		layer_conv_1d(
+		  filters, 5, 
+		  padding = "valid", activation = "relu", strides = 1
+		) %>%
+		layer_global_max_pooling_1d()
+
+	if (enriquecimento == 1) {
+		main_output <- layer_concatenate(c(ccn_out_3, ccn_out_4, ccn_out_5, entities_out, types_out)) %>% 
+				layer_dropout(0.2) %>%
+				layer_dense(units = 1, activation = 'sigmoid')
+
+		model <- keras_model(
+			inputs = c(main_input, auxiliary_input, auxiliary_input_types),
+			outputs = main_output
+		)
+	} else {
+		main_output <- layer_concatenate(c(ccn_out_3, ccn_out_4, ccn_out_5)) %>% 
+										layer_dropout(0.2) %>%
+										layer_dense(units = 1, activation = 'sigmoid')
+		model <- keras_model(
+			inputs = c(main_input),
+			outputs = main_output
+		)
+	}
+
+	# Compile model
+	model %>% compile(
+		loss = "binary_crossentropy",
+		optimizer = "adam",
+		metrics = "accuracy"
+	)
+
+	if (enriquecimento == 1) {
+		history <- model %>%
+			fit(
+			  x = list(dados_train_sequence, sequences, sequences_types),
+			  y = array(dados_train$resposta),
+			  batch_size = 64,
+			  epochs = epoca,
+			  callbacks = callbacks_list,
+			  validation_split = 0.2
+			)	
+		predictions <- model %>% predict(list(dados_test_sequence, sequences_test, sequences_test_types))
+	} else {
+		history <- model %>%
+			fit(
+			  x = list(dados_train_sequence),
+			  y = array(dados_train$resposta),
+			  batch_size = 64,
+			  epochs = epoca,
+			  callbacks = callbacks_list,
+			  validation_split = 0.2
+			)
+			
+		predictions <- model %>% predict(list(dados_test_sequence))
+	}
+	predictions2 <- round(predictions, 0)
+	matriz <- confusionMatrix(data = as.factor(predictions2), as.factor(dados_test$resposta), positive="1")
+	resultados <- addRowAdpater(resultados, paste0("Enriquecimento: ", enriquecimento, " - Early: ", early_stop), matriz)
+}
+resultados$F1
+resultados$Precision
+resultados$Recall
---
+++ experimentos/ds1/q2/cnn/WUDNO6374Y.R
@@ -0,0 +1,5 @@
+library("tools")
+
+bow <- 0
+source(file_path_as_absolute("experimentos/ds1/q2/getDadosQ2.R"))
+source(file_path_as_absolute("experimentos/ds1/cnn/WUDNO6374Y.R"))
