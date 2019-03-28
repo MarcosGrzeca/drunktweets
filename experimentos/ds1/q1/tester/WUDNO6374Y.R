6391bd61deb47897780989ca99e0db1d517fe406
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-03-28 10:33:27 -0300

    Tester WUDNO6374Y

---
+++ experimentos/ds1/q1/tester/WUDNO6374Y.R
@@ -0,0 +1,25 @@
+library(tools)
+source(file_path_as_absolute("ipm/loads.R"))
+early_stop <- 1
+
+epocas <- c(3,5,10)
+enriquecimentos <- c(0, 1)
+metricas <- c("acc", "val_loss")
+
+files <- c("experimentos/ds1/q1/cnn/WUDNO6374Y.R")
+
+for (file in files) {
+	redeDesc <- "WUDNO6374Y-DS1Q1"
+	for (epoca in epocas) {
+		for (metrica in metricas) {
+			for (enriquecimento in enriquecimentos) {
+				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
+				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
+				source(file_path_as_absolute(file))
+				logar("DS1-Q1", "Hidden", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc, file)
+			}
+		}
+	}
+}
+
+system("init 0")
