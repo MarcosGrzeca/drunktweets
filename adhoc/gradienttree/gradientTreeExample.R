447a5e58aa93aa29426f149c6b83872443dbdf0c
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-04-02 21:25:12 -0300

    Teste exprimento

--- adhoc/gradienttree/gradientTreeExample.R
+++ adhoc/gradienttree/gradientTreeExample.R
@@ -14,17 +14,16 @@ treinarTree <- function(X_train, y_train) {
                        subsample = 1
                       )
 
-	fit <- train(x = X_train,
-			y = y_train, 
+	fit <- train(X_train, y_train, 
 			method = "xgbTree", 
+			tuneGrid = xgbGrid,
 			trControl = trainControl(
 						method = "cv",
 						number = 5,
 						savePred = T,
 						allowParallel = TRUE,
 	  					returnData = FALSE
-  					),
-			tuneGrid = xgbGrid
+  					)
 			)
 	return (fit)
 }
@@ -53,42 +52,37 @@ library(doMC)
 library(mlbench)
 library(magrittr)
 
-CORES <- 8
+CORES <- 4
 registerDoMC(CORES)
 
 set.seed(10)
 split=0.80
 
-#for (year in 1:1){
-  try({
-  	power_plant = as.data.frame(read_excel("adhoc/gradienttree/Folds5x2_pp.xlsx"))
-  	View(power_plant)
-  	inTrain <- createDataPartition(y = power_plant$PE, p = split, list = FALSE)
-  	training <- power_plant[inTrain,]
- 	  testing <- power_plant[-inTrain,]
+power_plant = as.data.frame(read_excel("adhoc/gradienttree/Folds5x2_pp.xlsx"))
+inTrain <- createDataPartition(y = power_plant$PE, p = split, list = FALSE)
+training <- power_plant[inTrain,]
+testing <- power_plant[-inTrain,]
 
-  	X_train = xgb.DMatrix(as.matrix(training %>% select(-PE)))
-	  y_train = training$PE
-	  X_test = xgb.DMatrix(as.matrix(testing %>% select(-PE)))
-	  y_test = testing$PE
+X_train = xgb.DMatrix(as.matrix(training %>% select(-PE)))
+y_train = training$PE
+X_test = xgb.DMatrix(as.matrix(testing %>% select(-PE)))
+y_test = testing$PE
 
-	  treeModel <- treinarTree(X_train, y_train)
-	  treeModel$bestTune
-	  treeModel
+treeModel <- treinarTree(X_train, y_train)
+treeModel$bestTune
+treeModel
 
-	  pred <- predict(treeModel, X_test)
- 	  matrizTree <- confusionMatrix(data = pred, y_test, positive="1")
-	  resultados <- addRow(resultados, "2 GRAM - Entidades - Erro", matrizTree)
+pred <- predict(treeModel, X_test)
+  matrizTree <- confusionMatrix(data = pred, y_test, positive="1")
+resultados <- addRow(resultados, "2 GRAM - Entidades - Erro", matrizTree)
 
-	  options(repr.plot.width=8, repr.plot.height=4)
-	  my_data = as.data.frame(cbind(predicted = predicted,
-	                            observed = y_test))
-	  # Plot predictions vs test data
-	  ggplot(my_data,aes(predicted, observed)) + geom_point(color = "darkred", alpha = 0.5) + 
-	     geom_smooth(method=lm)+ ggtitle('Linear Regression ') + ggtitle("Extreme Gradient Boosting: Prediction vs Test Data") +
-	      xlab("Predecited Power Output ") + ylab("Observed Power Output") + 
-	        theme(plot.title = element_text(color="darkgreen",size=16,hjust = 0.5),
-	         axis.text.y = element_text(size=12), axis.text.x = element_text(size=12,hjust=.5),
-	         axis.title.x = element_text(size=14), axis.title.y = element_text(size=14))
-  })
-#}
+options(repr.plot.width=8, repr.plot.height=4)
+my_data = as.data.frame(cbind(predicted = predicted,
+                        observed = y_test))
+# Plot predictions vs test data
+ggplot(my_data,aes(predicted, observed)) + geom_point(color = "darkred", alpha = 0.5) + 
+ geom_smooth(method=lm)+ ggtitle('Linear Regression ') + ggtitle("Extreme Gradient Boosting: Prediction vs Test Data") +
+  xlab("Predecited Power Output ") + ylab("Observed Power Output") + 
+    theme(plot.title = element_text(color="darkgreen",size=16,hjust = 0.5),
+     axis.text.y = element_text(size=12), axis.text.x = element_text(size=12,hjust=.5),
+         axis.title.x = element_text(size=14), axis.title.y = element_text(size=14))
