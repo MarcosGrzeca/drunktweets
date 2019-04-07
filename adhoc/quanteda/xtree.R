b89b9e3833d03de0e7a5bfee993a8fef91967edb
Rstudio <username@ip-172-31-11-27.us-east-2.compute.internal>
2019-04-07 17:08:21 +0000

    V2

--- adhoc/quanteda/xtree.R
+++ adhoc/quanteda/xtree.R
@@ -4,6 +4,9 @@ library(lsa)
 library(readr)
 library(quanteda)
 
+library(doMC)
+registerDoMC(cores=4)
+
 source(file_path_as_absolute("utils/getDados.R"))
 source(file_path_as_absolute("baseline/dados.R"))
 source(file_path_as_absolute("utils/tokenizer.R"))
@@ -15,16 +18,15 @@ DATABASE <- "icwsm"
 dados <- getDadosAmazon()
 
 fbcorpus <- corpus(dados$textParser)
-fbdfm <- dfm(fbcorpus, remove=stopwords("english"), verbose=TRUE, stem = TRUE, remove_punct = TRUE)
+#fbdfm <- dfm(fbcorpus, remove=stopwords("english"), verbose=TRUE, stem = TRUE, remove_punct = TRUE)
+fbdfm <- dfm(fbcorpus, remove=stopwords("english"), verbose=TRUE, remove_punct = TRUE)
+fbdfm <- dfm_trim(fbdfm, min_docfreq = 2, verbose=TRUE)
 
 #dfm(x, verbose = TRUE, toLower = TRUE,
 #    removeNumbers = TRUE, removePunct = TRUE, removeSeparators = TRUE,
 #    removeTwitter = FALSE, stem = FALSE, ignoredFeatures = NULL,
 #    keptFeatures = NULL, language = "english", thesaurus = NULL,
 #    dictionary = NULL, valuetype = c("glob", "regex", "fixed"), ..
-
-
-fbdfm <- dfm_trim(fbdfm, min_docfreq = 2, verbose=TRUE)
 
 w2v <- readr::read_delim("adhoc/quanteda/code/FBvector.txt", 
                   skip=1, delim=" ", quote="",
@@ -48,8 +50,60 @@ for (i in 1:ndoc(fbdfm)){
   if (nrow(embed_vec)==0) embed[i,] <- 0
 }
 
-set.seed(123)
-training <- sample(1:nrow(fb), floor(.80 * nrow(fb)))
-test <- (1:nrow(fb))[1:nrow(fb) %in% training == FALSE]
+set.seed(10)
+training <- sample(1:nrow(dados), floor(.80 * nrow(dados)))
+test <- (1:nrow(dados))[1:nrow(dados) %in% training == FALSE]
 
 View(fbdfm[1,])
+
+library(xgboost)
+# converting matrix object
+X <- as(cbind(fbdfm, embed), "dgCMatrix")
+# parameters to explore
+tryEta <- c(1,2)
+tryDepths <- c(1,2,4)
+# placeholders for now
+bestEta=NA
+bestDepth=NA
+bestAcc=0
+
+for(eta in tryEta){
+  for(dp in tryDepths){ 
+    bst <- xgb.cv(data = X[training,], 
+                  label =  dados$resposta[training], 
+                  max.depth = dp,
+                  eta = eta, 
+                  nthread = 4,
+                  nround = 500,
+                  nfold=5,
+                  print_every_n = 100L,
+                  objective = "binary:logistic")
+    # cross-validated accuracy
+    acc <- 1-mean(tail(bst$evaluation_log$test_error_mean))
+    cat("Results for eta=",eta," and depth=", dp, " : ",
+        acc," accuracy.\n",sep="")
+    if(acc>bestAcc){
+      bestEta=eta
+      bestAcc=acc
+      bestDepth=dp
+    }
+  }
+}
+
+cat("Best model has eta=",bestEta," and depth=", bestDepth, " : ", bestAcc," accuracy.\n",sep="")
+
+# running best model
+rf <- xgboost(data = X[training,], 
+              label = dados$resposta[training], 
+              max.depth = bestDepth,
+              eta = bestEta, 
+              nthread = 4,
+              nround = 1000,
+              print_every_n=100L,
+              objective = "binary:logistic")
+
+# out-of-sample accuracy
+preds <- predict(rf, X[test,])
+
+
+cat("\nAccuracy on test set=", round(accuracy(preds>.50, fb$attacks[test]),3))
