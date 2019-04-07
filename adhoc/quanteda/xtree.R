a6363609e16deb70aea5bfea43d563d4889e9fd2
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-04-07 10:55:33 -0300

    Ajeitando vector

--- adhoc/quanteda/xtree.R
+++ adhoc/quanteda/xtree.R
@@ -13,3 +13,13 @@ source(file_path_as_absolute("utils/getDadosAmazon.R"))
 #Configuracoes
 DATABASE <- "icwsm"
 dados <- getDadosAmazon()
+
+fbcorpus <- corpus(dados$text_parser)
+fbdfm <- dfm(fbcorpus, remove=stopwords("english"), verbose=TRUE)
+fbdfm <- dfm_trim(fbdfm, min_docfreq = 2, verbose=TRUE)
+
+w2v <- readr::read_delim("adhoc/quanteda/code/FBvector.txt", 
+                  skip=1, delim=" ", quote="",
+                  col_names=c("word", paste0("V", 1:100)))
+
+w2v <- w2v[w2v$word %in% featnames(fbdfm),]
