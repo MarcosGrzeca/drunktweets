ca3257abd27477b2619f64ee1157b068a5a3ddb9
Rstudio <username@ip-172-31-13-239.us-east-2.compute.internal>
2019-04-09 23:19:57 +0000

    Apenas iterar testes

--- adhoc/quanteda/xtree_enriquecimento_lstm.R
+++ adhoc/quanteda/xtree_enriquecimento_lstm.R
@@ -64,58 +64,61 @@ training <- sample(1:nrow(dados), floor(.80 * nrow(dados)))
 test <- (1:nrow(dados))[1:nrow(dados) %in% training == FALSE]
 
 library(xgboost)
-# converting matrix object
-X <- as(cbind(embed,typesdfm,entidadesdfm), "dgCMatrix")
 
-# parameters to explore
-tryEta <- c(1,2,3)
-tryDepths <- c(1,2,4,6)
-# placeholders for now
-bestEta=NA
-bestDepth=NA
-bestAcc=0
-
-for(eta in tryEta){
-  for(dp in tryDepths){ 
-    bst <- xgb.cv(data = X[training,], 
-                  label =  dados$resposta[training], 
-                  max.depth = dp,
-                  eta = eta, 
-                  nthread = 4,
-                  nround = 500,
-                  nfold=5,
-                  print_every_n = 100L,
-                  objective = "binary:logistic")
-    # cross-validated accuracy
-    acc <- 1-mean(tail(bst$evaluation_log$test_error_mean))
-    cat("Results for eta=",eta," and depth=", dp, " : ",
-        acc," accuracy.\n",sep="")
-    if(acc>bestAcc){
-      bestEta=eta
-      bestAcc=acc
-      bestDepth=dp
+for (iteracao in 1:3) {
+  # converting matrix object
+  X <- as(cbind(embed,typesdfm,entidadesdfm), "dgCMatrix")
+  
+  # parameters to explore
+  tryEta <- c(1,2,3)
+  tryDepths <- c(1,2,4,6)
+  # placeholders for now
+  bestEta=NA
+  bestDepth=NA
+  bestAcc=0
+  
+  for(eta in tryEta){
+    for(dp in tryDepths){ 
+      bst <- xgb.cv(data = X[training,], 
+                    label =  dados$resposta[training], 
+                    max.depth = dp,
+                    eta = eta, 
+                    nthread = 4,
+                    nround = 500,
+                    nfold=5,
+                    print_every_n = 100L,
+                    objective = "binary:logistic")
+      # cross-validated accuracy
+      acc <- 1-mean(tail(bst$evaluation_log$test_error_mean))
+      cat("Results for eta=",eta," and depth=", dp, " : ",
+          acc," accuracy.\n",sep="")
+      if(acc>bestAcc){
+        bestEta=eta
+        bestAcc=acc
+        bestDepth=dp
+      }
     }
   }
+  
+  cat("Best model has eta=",bestEta," and depth=", bestDepth, " : ", bestAcc," accuracy.\n",sep="")
+  
+  # running best model
+  rf <- xgboost(data = X[training,], 
+                label = dados$resposta[training], 
+                max.depth = bestDepth,
+                eta = bestEta, 
+                nthread = 4,
+                nround = 500,
+                print_every_n=20L,
+                objective = "binary:logistic")
+  
+  # out-of-sample accuracy
+  preds <- predict(rf, X[test,])
+  
+  cat("\nAccuracy on test set=", round(accuracy(preds>.50, dados$resposta[test]),6))
+  cat("\nPrecision(1) on test set=", round(precision(preds>.50, dados$resposta[test]),6))
+  cat("\nRecall(1) on test set=", round(recall(preds>.50, dados$resposta[test]),6))
+  
+  #cat("\nPrecision(0) on test set=", round(precision(preds<.50, fb$attacks[test]==0),3))
+  #cat("\nRecall(0) on test set=", round(recall(preds<.50, fb$attacks[test]==0),3))
 }
-
-cat("Best model has eta=",bestEta," and depth=", bestDepth, " : ", bestAcc," accuracy.\n",sep="")
-
-# running best model
-rf <- xgboost(data = X[training,], 
-              label = dados$resposta[training], 
-              max.depth = bestDepth,
-              eta = bestEta, 
-              nthread = 4,
-              nround = 500,
-              print_every_n=20L,
-              objective = "binary:logistic")
-
-# out-of-sample accuracy
-preds <- predict(rf, X[test,])
-
-cat("\nAccuracy on test set=", round(accuracy(preds>.50, dados$resposta[test]),6))
-cat("\nPrecision(1) on test set=", round(precision(preds>.50, dados$resposta[test]),6))
-cat("\nRecall(1) on test set=", round(recall(preds>.50, dados$resposta[test]),6))
-
-#cat("\nPrecision(0) on test set=", round(precision(preds<.50, fb$attacks[test]==0),3))
-#cat("\nRecall(0) on test set=", round(recall(preds<.50, fb$attacks[test]==0),3))
