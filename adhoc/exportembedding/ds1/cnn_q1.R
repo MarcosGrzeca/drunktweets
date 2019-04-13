d5e8d572128b30e2c37c1399d030c39d037b6a40
Rstudio <username@ip-172-31-22-203.us-east-2.compute.internal>
2019-04-13 13:15:17 +0000

    Ajuste max words Q1

--- adhoc/exportembedding/ds1/cnn_q1.R
+++ adhoc/exportembedding/ds1/cnn_q1.R
@@ -10,7 +10,7 @@ dados <- getDadosBaselineByQ("q1")
 dados$textEmbedding <- removePunctuation(dados$textEmbedding)
 
 maxlen <- 38
-max_words <- 10000
+max_words <- 7860
 
 tokenizer <-  text_tokenizer(num_words = max_words) %>%
               fit_text_tokenizer(dados$textEmbedding)
