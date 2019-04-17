efbb06e8678958fb1dfb201eff1a1d783896e8b2
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-04-17 11:19:20 -0300

    Embeddings DS1

--- ensemblev2/run/networkglove/exp3.R
+++ ensemblev2/run/networkglove/exp3.R
@@ -12,11 +12,11 @@ source(file_path_as_absolute("utils/tokenizer.R"))
 source(file_path_as_absolute("ipm/glove/load.R"))
 
 #Section: Dados classificar
-dados <- getDadosBaselineByQ("q1")
+dados <- getDadosBaselineByQ("q3")
 try({
 	save <- 1
 	maxlen <- 38
-	max_words <- 7574
+	max_words <- 3080
 	source(file_path_as_absolute("ensemblev2/run/networkglove/ipmtrain_cnn_glove.R"))
 	#source(file_path_as_absolute("ensemblev2/git.R"))
 })
