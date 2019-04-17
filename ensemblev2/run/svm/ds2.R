ba3d69eef894538662a41c81a10c554d15c22e47
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-04-17 14:36:19 -0300

    Update ds2.R

--- ensemblev2/run/svm/ds2.R
+++ ensemblev2/run/svm/ds2.R
@@ -4,7 +4,7 @@ datasetFile <-"rdas/2gram-entidades-hora-erro-semkeywords.Rda"
 baseResultsFiles <- "ensemblev2/resultados/ds2/"
 baseResampleFiles <- "ensemblev2/resample/ds2/"
 
-coreCustomizado <- 3
+coreCustomizado <- 2
 
 try({
 	source(file_path_as_absolute("ensemblev2/run/svm/core.R"))
