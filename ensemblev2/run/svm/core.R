0e5831f3003ad2a3be2da95c13668cecb232f9c6
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-04-16 22:07:58 -0300

    Ensemble v2

---
+++ ensemblev2/run/svm/core.R
@@ -0,0 +1,36 @@
+library(caret)
+
+treinarPoly <- function(data_train){
+	fit <- train(x = subset(data_train, select = -c(resposta)),
+			y = data_train$resposta, 
+			method = "svmPoly", 
+			trControl = trainControl(method = "cv", number = 5, savePred=T, classProbs = TRUE))
+	return (fit)
+}
+
+library(tools)
+library(caret)
+library(doMC)
+library(mlbench)
+library(magrittr)
+
+CORES <- 5
+registerDoMC(CORES)
+
+set.seed(10)
+split=0.80
+for (year in 1:5){
+  try({
+	load(file = datasetFile)
+	levels(maFinal$resposta) <- make.names(levels(factor(maFinal$resposta)))
+	trainIndex <- readRDS(file = paste0(baseResampleFiles, "trainIndex", year, ".rds"))
+	data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+	data_test <- maFinal[-trainIndex,]
+	treegram25NotNull <- treinarPoly(data_train)
+
+	pred <- predict(treegram25NotNull, subset(data_test, select = -c(resposta)), type = "prob") %>% 
+  			mutate('class'=names(.)[apply(., 1, which.max)])
+
+	saveRDS(pred$X1, file = paste0(baseResultsFiles, "svm", year, ".rds"))
+  })
+}
---
+++ ensemblev2/run/svm/exp1.R
@@ -0,0 +1,8 @@
+library(tools)
+
+try({
+	datasetFile <-"ensemble/datasets/exp1/2-Gram-dbpedia-types-enriquecimento-info-q1-not-null_info_entidades.Rda"
+	baseResultsFiles <- "ensemblev2/resultados/exp1/"
+	baseResampleFiles <- "ensemblev2/resample/exp1/"
+	source(file_path_as_absolute("ensemblev2/run/svm/core.R"))
+})
