ca0c8ccaa007bd53f05f82c542f1ecf7757331eb
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-04-16 22:29:18 -0300

    Update exp2.R

--- ensemblev2/run/svm/exp2.R
+++ ensemblev2/run/svm/exp2.R
@@ -1,7 +1,7 @@
 library(tools)
 
 try({
-	datasetFile <-"ensemblev2/datasets/exp2/2-Gram-dbpedia-types-enriquecimento-info-q2-not-null_info_entidades.Rda"
+	datasetFile <-"ensemble/datasets/exp2/2-Gram-dbpedia-types-enriquecimento-info-q2-not-null_info_entidades.Rda"
 	baseResultsFiles <- "ensemblev2/resultados/exp2/"
 	baseResampleFiles <- "ensemblev2/resample/exp2/"
 	source(file_path_as_absolute("ensemblev2/run/svm/core.R"))
