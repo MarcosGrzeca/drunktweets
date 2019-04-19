54b84cd6a718f10e411a1a5b6b9b6fa88e8d6fe8
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-04-18 22:00:36 -0300

    BORa

--- ensemblev2/run/svm/ds2.R
+++ ensemblev2/run/svm/ds2.R
@@ -4,9 +4,8 @@ datasetFile <-"rdas/2gram-entidades-hora-erro-semkeywords.Rda"
 baseResultsFiles <- "ensemblev2/resultados/ds2/"
 baseResampleFiles <- "ensemblev2/resample/ds2/"
 
-coreCustomizado <- 2
-
-try({
+# try({
+	coreCustomizado <- 5
 	source(file_path_as_absolute("ensemblev2/run/svm/core.R"))
 	source(file_path_as_absolute("ensemblev2/git.R"))
-})
+# })
--- ensemblev2/run/svm/ds3.R
+++ ensemblev2/run/svm/ds3.R
@@ -4,8 +4,8 @@ datasetFile <- "amazon/rdas/2gram-entidades-erro.Rda"
 baseResultsFiles <- "ensemblev2/resultados/ds3/"
 baseResampleFiles <- "ensemblev2/resample/ds3/"
 
-try({
-	coreCustomizado <- 10
+# try({
+	coreCustomizado <- 8
 	source(file_path_as_absolute("ensemblev2/run/svm/core.R"))
 	source(file_path_as_absolute("ensemblev2/git.R"))
-})
+# })
