f9e33fc2931f21fb389101f4bbafaef2615df75d
root <root@ip-172-31-0-219.us-east-2.compute.internal>
2019-04-21 18:40:08 +0000

    Teste

--- ensemblev2/comparador/comparador_oficial.R
+++ ensemblev2/comparador/comparador_oficial.R
@@ -1,19 +1,23 @@
 library(caret)
 library(dplyr)
 
-expName <- "exp1"
+expName <- "ds3"
 
-for (year in 1:1) {
-  year <- 1
+for (year in 1:5) {
 	svmResults <- readRDS(file = paste0("ensemblev2/resultados/", expName, "/svm", year, ".rds"))
 	xgboost <- readRDS(file = paste0("ensemblev2/resultados/", expName, "/xgboost", year, ".rds"))
 	nnResults <- readRDS(file = paste0("ensemblev2/resultados/", expName, "/neuralprob", year, ".rds"))
 
 	if (expName == "exp1") {
 		datasetFile <-"ensemble/datasets/exp1/2-Gram-dbpedia-types-enriquecimento-info-q1-not-null_info_entidades.Rda"
+  } else if (expName == "exp2") {
+    datasetFile <-"ensemble/datasets/exp2/2-Gram-dbpedia-types-enriquecimento-info-q2-not-null_info_entidades.Rda"
+  } else if (expName == "exp3") {
+    datasetFile <-"ensemble/datasets/exp3/2-Gram-dbpedia-types-enriquecimento-info-q3-not-null_info_entidades.Rda"
+  } else if (expName == "ds2") {
+    datasetFile <-"chat/rdas/2gram-entidades-erro-sem-key-words.Rda"
 	} else if (expName == "ds3") {
-		#Verificar
-		# load("amazon/rdas/2gram-entidades-erro.Rda")
+	  datasetFile <-"amazon/rdas/2gram-entidades-erro.Rda"
 	}
 	
 	load(datasetFile)
@@ -22,17 +26,13 @@ for (year in 1:1) {
 	data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
 	data_test <- maFinal[-trainIndex,]
 	
-	str(svmResults)
-	str(nnResults)
-	str(xgboost)
+	#bigDataFrame <- bind_cols(list(as.numeric(as.character(svmResults)), as.numeric(as.character(nnResults)), as.numeric(as.character(xgboost))))
+	#pred <- round(rowMeans(bigDataFrame),0)
 	
-	bigDataFrame <- bind_cols(list(as.numeric(as.character(svmResults)), as.numeric(as.character(nnResults)), as.numeric(as.character(xgboost))))
-	View(bigDataFrameSum)
+	bigDataFrame <- as.numeric(as.character(xgboost))
+	pred <- round(bigDataFrame,0)
 	
-	bigDataFrameSum <- rowSums(bigDataFrame)
-	
-	result <- bigDataFrameSum / 3
-	pred <- round(result,0)
+	pred
 		
 	matriz <- confusionMatrix(data = as.factor(pred), as.factor(data_test$resposta), positive="1")
 	print(matriz$byClass["F1"])
