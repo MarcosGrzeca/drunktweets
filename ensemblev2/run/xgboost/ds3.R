345e9817d095a07b47820162310af77c0e0e1d6e
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-04-20 22:07:53 -0300

    Update ds3.R

--- ensemblev2/run/xgboost/ds3.R
+++ ensemblev2/run/xgboost/ds3.R
@@ -4,6 +4,7 @@ source(file_path_as_absolute("ipm/experimenters.R"))
 source(file_path_as_absolute("utils/getDados.R"))
 source(file_path_as_absolute("baseline/dados.R"))
 source(file_path_as_absolute("utils/tokenizer.R"))
+source(file_path_as_absolute("utils/getDadosAmazon.R"))
 
 baseResultsFiles <- "ensemblev2/resultados/ds3/"
 baseResampleFiles <- "ensemblev2/resample/ds3/"
