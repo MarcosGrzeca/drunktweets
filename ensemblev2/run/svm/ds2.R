904e0a85e7a2d99a415bfae9b8b93121071383d8
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-04-21 16:12:52 -0300

    Update ds2.R

--- ensemblev2/run/svm/ds2.R
+++ ensemblev2/run/svm/ds2.R
@@ -1,6 +1,6 @@
 library(tools)
 
-datasetFile <-"rdas/2gram-entidades-hora-erro-semkeywords.Rda"
+datasetFile <-"chat/rdas/2gram-entidades-erro-sem-key-words.Rda"
 baseResultsFiles <- "ensemblev2/resultados/ds2/"
 baseResampleFiles <- "ensemblev2/resample/ds2/"
 
