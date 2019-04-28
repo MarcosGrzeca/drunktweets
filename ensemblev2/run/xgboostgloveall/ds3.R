0baee4e23861f5cc42dcc0e6069e2bd9016e8e6f
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-04-27 21:04:28 -0300

    Zole experimentos

---
+++ ensemblev2/run/xgboostgloveall/ds3.R
@@ -0,0 +1,18 @@
+library(tools)
+
+source(file_path_as_absolute("ipm/experimenters.R"))
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("baseline/dados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+source(file_path_as_absolute("utils/getDadosAmazon.R"))
+
+baseResultsFiles <- "ensemblev2/resultados/ds3/"
+baseResampleFiles <- "ensemblev2/resample/ds3/"
+embeddingFile <- "adhoc/exportembedding/glove_50epocas_5l.txt"
+
+dados <- getDadosAmazon()
+
+try({
+	source(file_path_as_absolute("ensemblev2/run/xgboost/xgboost_core.R"))
+	#source(file_path_as_absolute("ensemblev2/git.R"))
+})
---
+++ ensemblev2/run/xgboostgloveall/exp2.R
@@ -0,0 +1,20 @@
+library(tools)
+
+source(file_path_as_absolute("ipm/experimenters.R"))
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("baseline/dados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+
+baseResultsFiles <- "ensemblev2/resultados/exp2/"
+baseResampleFiles <- "ensemblev2/resample/exp2/"
+# embeddingFile <- "adhoc/exportembedding/ds1/q2/cnn_10_epocas.txt"
+embeddingFile <- "adhoc/exportembedding/ds3/cnn_10_epocas.txt"
+
+dados <- getDadosBaselineByQ("q2")
+
+try({
+	maxlen <- 38
+	max_words <- 4405
+	source(file_path_as_absolute("ensemblev2/run/xgboost/xgboost_core.R"))
+	#source(file_path_as_absolute("ensemblev2/git.R"))
+})
