e9c8238c66c268586d22d8182811179ecde37d51
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-04-28 10:31:26 -0300

    glove_50epocas_0l_sem_stopwords.txt

--- ensemblev2/run/xgboostgloveall/exp2.R
+++ ensemblev2/run/xgboostgloveall/exp2.R
@@ -7,7 +7,7 @@ source(file_path_as_absolute("utils/tokenizer.R"))
 
 baseResultsFiles <- "ensemblev2/resultados/exp2/"
 baseResampleFiles <- "ensemblev2/resample/exp2/"
-embeddingFile <- "adhoc/exportembedding/glove_50epocas_0l.txt"
+embeddingFile <- "adhoc/exportembedding/glove_50epocas_0l_sem_stopwords.txt"
 
 dados <- getDadosBaselineByQ("q2")
 
