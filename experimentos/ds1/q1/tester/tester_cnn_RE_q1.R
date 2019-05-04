a652fcd1aad32d098d4546c350a09fd9451e9ebb
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-05-04 17:33:29 -0300

    Perdido na vida

---
+++ experimentos/ds1/q1/tester/tester_cnn_RE_q1.R
@@ -0,0 +1,25 @@
+library(tools)
+source(file_path_as_absolute("ipm/loads.R"))
+
+epocas <- c(3,5,10)
+enriquecimentos <- c(0, 1)
+metricas <- c("acc", "val_loss")
+early_stop <- 1
+
+files <- c("experimentos/ds1/q1/cnn/cnn_random_embeddings_bow_q1.R")
+
+for (file in files) {
+	redeDesc <- "CNN_RE_DS1Q1"
+	for (epoca in epocas) {
+		for (metrica in metricas) {
+			for (enriquecimento in enriquecimentos) {
+				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
+				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
+				source(file_path_as_absolute(file))
+				logar("DS1-Q1", "Hidden", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc, file)
+			}
+		}
+	}
+}
+
+system("init 0")
