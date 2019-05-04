0834f4c489f81dab62f045c2ce50da1bf595b9db
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-05-04 18:50:32 -0300

    DS2 RE

---
+++ experimentos/ds2/tester_cnn_RE_ds2.R
@@ -0,0 +1,28 @@
+library(tools)
+source(file_path_as_absolute("ipm/loads.R"))
+
+epocas <- c(5)
+enriquecimentos <- c(0, 1)
+metricas <- c("val_loss")
+early_stop <- 1
+
+bow <- 1
+source(file_path_as_absolute("experimentos/ds2/getDados.R"))
+files <- c("experimentos/ds2/cnn/cnn_random_embeddings.R")
+
+try({
+  for (file in files) {
+  	redeDesc <- "CNN_RE_DS2"
+  	for (epoca in epocas) {
+  		for (metrica in metricas) {
+  			for (enriquecimento in enriquecimentos) {
+  				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
+  				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
+  				source(file_path_as_absolute(file))
+  				logar("DS2", "Hidden", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc, file)
+  			}
+  		}
+  	}
+  }
+  system("init 0")
+})
