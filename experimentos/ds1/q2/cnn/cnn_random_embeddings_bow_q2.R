b622d92bfa9ca2f5c92c7fdb992f5015c184d0ca
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-05-04 18:16:22 -0300

    RE DS1-Q2

---
+++ experimentos/ds1/q2/cnn/cnn_random_embeddings_bow_q2.R
@@ -0,0 +1,5 @@
+library("tools")
+
+bow <- 1
+source(file_path_as_absolute("experimentos/ds1/q2/getDadosQ2.R"))
+source(file_path_as_absolute("experimentos/ds1/cnn/cnn_random_embeddings.R"))
---
+++ experimentos/ds1/q2/tester/tester_cnn_RE_q2.R
@@ -0,0 +1,25 @@
+library(tools)
+source(file_path_as_absolute("ipm/loads.R"))
+
+epocas <- c(5)
+enriquecimentos <- c(0, 1)
+metricas <- c("val_loss")
+early_stop <- 1
+
+files <- c("experimentos/ds1/q2/cnn/cnn_random_embeddings_bow_q2.R")
+
+for (file in files) {
+	redeDesc <- "CNN_RE_DS1Q2"
+	for (epoca in epocas) {
+		for (metrica in metricas) {
+			for (enriquecimento in enriquecimentos) {
+				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
+				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
+				source(file_path_as_absolute(file))
+				logar("DS1-Q2", "Hidden", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc, file)
+			}
+		}
+	}
+}
+
+system("init 0")
