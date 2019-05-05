906af73e5d40c21e0999e2a714fac3d9b31a558e
root <root@ip-172-31-5-255.us-east-2.compute.internal>
2019-05-05 02:45:04 +0000

    Teste

--- experimentos/ds1/cnn/cnn_skipgrams.R
+++ experimentos/ds1/cnn/cnn_skipgrams.R
@@ -7,19 +7,26 @@ w2v <- readr::read_delim("adhoc/exportembedding/new_skipgrams_10_epocas_5l.txt",
                          skip=1, delim=" ", quote="",
                          col_names=c("word", paste0("V", 1:100)))
 
-a <- w2v[w2v$word %in% "tequila", 2:101]
-
-embedding_dims <- 100
-embedding_matrix <- array(0, c(max_words, embedding_dims))
-
 for (word in names(word_index)) {
   index <- word_index[[word]]
   if (index < max_words) {
   	embedding_vector <- w2v[w2v$word %in% word, 2:101]
+  	embedding_vector
+  	View(embedding_vector)
     if (!is.null(embedding_vector))
-      embedding_matrix[index+1,] <- embedding_vector
+      embedding_matrix[index+1,] <- as.vector(embedding_vector)
   }
 }
+
+str(embedding_matrix)
+
+aa <- as.vector(embedding_vector)
+
+View(aa)
+
+aa
+
+index
 
 library(tools)
 
