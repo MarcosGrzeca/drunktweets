42bd04a708c5a3bdbddc0527dbbe58401b913654
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-05-07 16:17:09 -0300

    zole ajeitar

--- experimentos/v2/re/q1.R
+++ experimentos/v2/re/q1.R
@@ -27,7 +27,7 @@ for (file in files) {
 				resultados <- data.frame(matrix(ncol = 4, nrow = 0))
 				names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
 				source(file_path_as_absolute(file))
-				logar("DS1-Q1", "SKG", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc, file)
+				logar("DS1-Q1", "Hidden", "CNN", epoca, 1, metrica, enriquecimento, resultados, model_to_json(model), redeDesc, file)
 			}
 		}
 	}
