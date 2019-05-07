d86d7b485783e85b3da460d965feaa7a54f8ad0c
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-05-07 13:49:28 -0300

    Refazendo exp DL

--- experimentos/ds1/sequence_generate_with_bow.R
+++ experimentos/ds1/sequence_generate_with_bow.R
@@ -15,13 +15,13 @@ try({
 
 	data <- pad_sequences(sequences, maxlen = maxlen)
 
-	tokenizer_entities <- text_tokenizer(num_words = 1000) %>%
+	tokenizer_entities <- text_tokenizer(num_words = 50) %>%
 	fit_text_tokenizer(dados$entidades)
 
 	vocabEntitiesLenght <- length(tokenizer_entities$word_index)
 	dados$sequences <- texts_to_sequences(tokenizer_entities, dados$entidades)
 
-	tokenizer_types <- text_tokenizer(num_words = 1000) %>%
+	tokenizer_types <- text_tokenizer(num_words = 50) %>%
 	fit_text_tokenizer(dados$types)
 	vocabTypesLenght <- length(tokenizer_types$word_index)
 	dados$sequences_types <- texts_to_sequences(tokenizer_types, dados$types)
