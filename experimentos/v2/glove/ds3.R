112b271e66b18bafccefa63771f8027c8b8d43e5
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-05-09 20:06:28 -0300

    LA vamos nos

--- experimentos/v2/glove/ds3.R
+++ experimentos/v2/glove/ds3.R
@@ -17,7 +17,7 @@ files <- c("experimentos/v2/glove/core/CNNGlove.R")
 fileGetDados <- "experimentos/ds3/getDadosDS3.R"
 
 for (file in files) {
-	redeDesc <- "V4_CNNGloveBowDS3-20"
+	redeDesc <- "V6_CNNGloveBowDS3-20"
 	for (epoca in epocas) {
 		for (metrica in metricas) {
 			for (enriquecimento in enriquecimentos) {
--- experimentos/v2/glove/q1.R
+++ experimentos/v2/glove/q1.R
@@ -19,7 +19,7 @@ files <- c("experimentos/v2/glove/core/CNNGlove.R")
 fileGetDados <- "experimentos/ds1/sequence_generate_with_bow.R"
 
 for (file in files) {
-	redeDesc <- "V4_CNNGloveBowDS1-Q1-20"
+	redeDesc <- "V6_CNNGloveBowDS1-Q1-20"
 	for (epoca in epocas) {
 		for (metrica in metricas) {
 			for (enriquecimento in enriquecimentos) {
