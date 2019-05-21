0e652a8a38e1518c637bf98bbc5150f8d68f1b75
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-05-20 21:12:50 -0300

    Update requires.R

--- experimentos/v3/glove/requires.R
+++ experimentos/v3/glove/requires.R
@@ -1,7 +1,7 @@
 # files <- c("experimentos/v3/glove/core/CNNGloveSemBow.R")
 files <- c("experimentos/v3/glove/core/CNNGloveSemBow16.R")
 
-PREFX <- "VVFinal_"
+PREFX <- "MAFinal_"
 
 # if (questionAval == "q1") {
 # 	fileGetDados <- "experimentos/ds1/sequence_generate_with_bow_todas_entidades.R"
