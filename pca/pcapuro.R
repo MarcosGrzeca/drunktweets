ce8e9b8d99672d515b420d82ae945847fa7ae753
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-06-01 17:11:10 -0300

    Testes PCA over sequences

--- pca/pcapuro.R
+++ pca/pcapuro.R
@@ -7,14 +7,14 @@ source(file_path_as_absolute("pca/loader.R"))
 library("factoextra")
 library("FactoMineR")
 
-res.pca <- PCA(embedding_matrixTwo,  graph = FALSE)
-res.pca <- PCA(df,  graph = FALSE)
-fviz_pca_ind(res.pca, col.ind = "cos2",
+pca <- PCA(train_sequences,  graph = FALSE)
+
+fviz_pca_ind(pca, col.ind = "cos2",
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
              repel = TRUE # Avoid text overlapping (slow if many points)
 )
 
 
-res.dist <- get_dist(df, stand = TRUE, method = "pearson")
-fviz_dist(res.dist, 
-          gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+# res.dist <- get_dist(df, stand = TRUE, method = "pearson")
+# fviz_dist(res.dist, 
+#           gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
--- pca/rtsne.R
+++ pca/rtsne.R
@@ -1,59 +1,17 @@
 library(tools)
-library(text2vec)
 
-library(text2vec)
-
-library(tools)
-library(text2vec)
-library(data.table)
-library(SnowballC)
-library(keras)
-
-source(file_path_as_absolute("utils/getDados.R"))
-source(file_path_as_absolute("baseline/dados.R"))
-source(file_path_as_absolute("utils/tokenizer.R"))
-
-dadosTreinarEmbeddings <- getDadosWordEmbeddings()
-wiki <- dadosTreinarEmbeddings$textEmbedding
-
-# create vocabulary
-tokens <- space_tokenizer(wiki)
-it = itoken(tokens)
-stop_words <- tm::stopwords("en")
-vocab <- create_vocabulary(it, stopwords = stop_words)
-#vocab <- prune_vocabulary(vocab)
-
-#vectorizer = vocab_vectorizer(vocab, grow_dtm = F, skip_grams_window = 5)
-vectorizer <- vocab_vectorizer(vocab)
-
-tcm <- create_tcm(it, vectorizer, skip_grams_window = 5L)
-
-glove <- GlobalVectors$new(word_vectors_size = 100, vocabulary = vocab, x_max = 10)
-wv_main <- fit_transform(tcm, glove, n_iter = 25)
-wv_context <- glove$components
-word_vectors <- wv_main + t(wv_context)
-save(word_vectors, file = "ipm/embeddings/skipgram_glove_without_stopwords.Rda")
+DESC <- "Exp1 GloVe- CNN + Semantic Enrichment + Word embeddings"
+source(file_path_as_absolute("pca/loader.R"))
 
 library(Rtsne)
 library(ggplot2)
 library(plotly)
 
-#tsne <- Rtsne(word_vectors, perplexity = 50, pca = TRUE)
-tsne <- Rtsne(word_vectors[1:1500,], perplexity = 100, pca = TRUE)
+tsne <- Rtsne(train_sequences, perplexity = 100, pca = TRUE)
 
 tsne_plot <- tsne$Y %>%
   as.data.frame() %>%
-  mutate(word = row.names(word_vectors[1:1500,])) %>%
+  mutate(word = row.names(train_sequences)) %>%
   ggplot(aes(x = V1, y = V2, label = word)) + 
   geom_text(size = 3)
 tsne_plot
-
-
-find_similar_words <- function(word, embedding_matrix, n = 5) {
-  similarities <- embedding_matrix[word, , drop = FALSE] %>%
-    sim2(embedding_matrix, y = ., method = "cosine")
-  
-  similarities[,1] %>% sort(decreasing = TRUE) %>% head(n)
-}
-
-find_similar_words("beer", word_vectors, 20)
