03d775fb762f1470c1d18769c75f12b260cb621b
root <marcos-a-g@hotmail.com>
2019-06-02 03:37:14 +0000

    TESTE

--- pca/cnntest_1.R
+++ pca/cnntest_1.R
@@ -16,11 +16,12 @@ set.seed(10)
 split=0.80
 
 for (year in 1:20) {
+  year <- 1
 	trainIndex <- readRDS(file = paste0(baseResampleFiles, "trainIndex", year, ".rds"))
 	# trainIndex <- createDataPartition(dados$resposta, p=0.8, list=FALSE)
 	
-	dados_train <- dados[ trainIndex,]
-	dados_test <- dados[-trainIndex,]
+	dados_train <- data[ trainIndex,]
+	dados_test <- data[-trainIndex,]
 
 	train_sequences <- pca_entities$x[ trainIndex,]
 	train_sequences_types <- pca_types$x[ trainIndex,]
@@ -67,7 +68,7 @@ for (year in 1:20) {
 	embedding_input <-  main_input %>% 
 						layer_embedding(input_dim = vocab_size, output_dim = embedding_dims, input_length = maxlen)
 	
-	ccn_out_3 <- 	edding_input %>% 
+	ccn_out_3 <- 	embedding_input %>% 
 					layer_conv_1d(
 						filters, 3,
 						padding = "valid", activation = "relu", strides = 1
@@ -97,33 +98,11 @@ for (year in 1:20) {
 
 	auxilary_output <- layer_concatenate(c(auxiliary_input_entidades, auxiliary_input_types))
 
-	filters_auxiliary <- 30
-	auxiliary_ccn_out_3 <- 	auxilary_output %>% 
-							layer_conv_1d(
-								filters_auxiliary, 3,
-								padding = "valid", activation = "relu", strides = 1
-							) %>%
-							layer_global_max_pooling_1d()
-
-	auxiliary_ccn_out_4 <- 	auxilary_output %>% 
-							layer_conv_1d(
-								filters_auxiliary, 4, 
-								padding = "valid", activation = "relu", strides = 1
-							) %>%
-							layer_global_max_pooling_1d()
-
-	auxiliary_ccn_out_5 <- 	embedding_input %>% 
-							layer_conv_1d(
-								filters_auxiliary, 5, 
-								padding = "valid", activation = "relu", strides = 1
-							) %>%
-							layer_global_max_pooling_1d()
-
-	cnn_output <- 	layer_concatenate(c(auxiliary_ccn_out_3, auxiliary_ccn_out_4, auxiliary_ccn_out_5)) %>% 
+	auxiliar <- 	auxilary_output %>% 
 					layer_dropout(0.2) %>%
 					layer_dense(units = 8, activation = "relu", kernel_regularizer = regularizer_l2(0.001))
 
-	main_output <- 	layer_concatenate(c(cnn_output, cnn_output)) %>% 
+	main_output <- 	layer_concatenate(c(cnn_output, auxiliar)) %>% 
 					layer_dense(units = 24, activation = "relu", kernel_regularizer = regularizer_l2(0.001)) %>%
 					layer_dense(units = 1, activation = 'sigmoid')
 
@@ -145,18 +124,19 @@ for (year in 1:20) {
 
 	history <- 	model %>%
 				fit(
-					x = list(dados_train_sequence, train_sequences, train_sequences_types),
-					y = array(dados_train$resposta),
+					x = list(dados_train, train_sequences, train_sequences_types),
+					y = array(dados[trainIndex,]$resposta),
 					batch_size = FLAGS$batch_size,
 					epochs = 5,
 					callbacks = callbacks_list,
 					validation_split = 0.2
 				)
 
-	predictions <- model %>% predict(list(dados_test_sequence, test_sequences, test_sequences_types))
+	predictions <- model %>% predict(list(dados_test, test_sequences, test_sequences_types))
 	predictions2 <- round(predictions, 0)
 
-    matriz <- confusionMatrix(data = as.factor(predictions2), as.factor(dados_test$resposta), positive="1")
+    matriz <- confusionMatrix(data = as.factor(predictions2), as.factor(dados[-trainIndex,]$resposta), positive="1")
+    matriz
     resultados <- addRowAdpater(resultados, "Com enriquecimento", matriz)
 }
 
