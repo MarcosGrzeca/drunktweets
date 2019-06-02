3cd134cf6c60ca000f5b82541bfc3204f47a99e8
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-06-02 00:28:37 -0300

    Update loader.R

--- pca/loader.R
+++ pca/loader.R
@@ -9,40 +9,55 @@ source(file_path_as_absolute("utils/tokenizer.R"))
 #Section: Dados classificar
 dados <- getDadosAmazon()
 
-save <- 1
 maxlen <- 38
 max_words <- 9052
 
-tokenizer_entities <- 	text_tokenizer(num_words = 1002) %>%
+tokenizer <-  text_tokenizer(num_words = max_words) %>%
+              fit_text_tokenizer(dados$textEmbedding)
+
+sequences <- texts_to_sequences(tokenizer, dados$textEmbedding)
+word_index = tokenizer$word_index
+
+vocab_size <- length(word_index)
+vocab_size <- vocab_size + 1
+vocab_size
+
+cat("Found", length(word_index), "unique tokens.\n")
+data <- pad_sequences(sequences, maxlen = maxlen)
+
+tokenizer_entities <- 	text_tokenizer(num_words = 15) %>%
 						fit_text_tokenizer(dados$entidades)
 vocabEntitiesLenght <- length(tokenizer_entities$word_index)
 dados$sequences <- texts_to_sequences(tokenizer_entities, dados$entidades)
 
-tokenizer_types <- 	text_tokenizer(num_words = 200) %>%
+tokenizer_types <- 	text_tokenizer(num_words = 300) %>%
 					fit_text_tokenizer(dados$types)
 vocabTypesLenght <- length(tokenizer_types$word_index)
 dados$sequences_types <- texts_to_sequences(tokenizer_types, dados$types)
+
+max_sequence <- max(sapply(dados$sequences[lengths(dados$sequences) > 0], max))
+max_sequence_types <- max(sapply(dados$sequences_types[lengths(dados$sequences_types) > 0], max))
+
+new_entities <- vectorize_sequences(dados$sequences, dimension = max_sequence)
+new_types <- vectorize_sequences(dados$sequences_types, dimension = max_sequence_types)
+
+pca_entities <- prcomp(new_entities, scale = FALSE)
+#pca_entities$x
+pca_types <- prcomp(new_types, scale = FALSE)
+#pca_entities$x
 
 library(caret)
 library(tools)
 library(keras)
 
+##SEPARANDO DADOS
 trainIndex <- createDataPartition(dados$resposta, p=0.8, list=FALSE)
 
-dados_train <- dados[ trainIndex,]
-dados_test <- dados[-trainIndex,]
+# dados_train <- dados[ trainIndex,]
+# dados_test <- dados[-trainIndex,]
 
-max_sequence <- max(sapply(dados_train$sequences[lengths(dados_train$sequences) > 0], max))
-max_sequence_types <- max(sapply(dados_train$sequences_types[lengths(dados_train$sequences_types) > 0], max))
+# train_sequences <- pca_entities$x[ trainIndex,]
+# train_sequences_types <- pca_types$x[ trainIndex,]
 
-train_sequences <- vectorize_sequences(dados_train$sequences, dimension = max_sequence)
-train_sequences_types <- vectorize_sequences(dados_train$sequences_types, dimension = max_sequence_types)
-
-test_sequences <- vectorize_sequences(dados_test$sequences, dimension = max_sequence)
-test_sequences_types <- vectorize_sequences(dados_test$sequences_types, dimension = max_sequence_types)
-
-tokenizerBow <- text_tokenizer(num_words = max_words) %>%
-		fit_text_tokenizer(dados$textParser)
-
-dataframebow_train <- texts_to_matrix(tokenizerBow, dados_train$textParser, mode = "binary")
-dataframebow_test  <- texts_to_matrix(tokenizerBow, dados_test$textParser,  mode = "binary")
+# test_sequences <- pca_entities$x[-trainIndex,]
+# test_sequences_types <- pca_types$x[-trainIndex,]
