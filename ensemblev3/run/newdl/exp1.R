d086ec971afe9f5b53bf1eb09428a89bc7a92d96
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-06-20 23:29:46 -0300

    New ensemble

---
+++ ensemblev3/run/newdl/exp1.R
@@ -0,0 +1,19 @@
+library(tools)
+
+baseResultsFiles <- "ensemblev3/resultados/exp1/"
+baseResampleFiles <- "ensemblev2/resample/exp1/"
+
+source(file_path_as_absolute("ipm/experimenters.R"))
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("baseline/dados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+source(file_path_as_absolute("ipm/glove/load.R"))
+
+#Section: Dados classificar
+dados <- getDadosBaselineByQ("q1")
+try({
+	save <- 1
+	maxlen <- 38
+	max_words <- 7574
+	source(file_path_as_absolute("ensemblev3/run/networkglove/ipmtrain_cnn_glove.R"))
+})
