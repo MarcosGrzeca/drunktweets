3c67e87827419b1a636ad87443484f806b784079
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-06-22 10:25:11 -0300

    Update ds2.R

--- ensemblev3/run/newdl/ds2.R
+++ ensemblev3/run/newdl/ds2.R
@@ -1,7 +1,7 @@
 library(tools)
 
-baseResultsFiles <- "ensemblev3/resultados/exp3/newdl/"
-baseResampleFiles <- "ensemblev2/resample/exp3/"
+baseResultsFiles <- "ensemblev3/resultados/ds2/newdl/"
+baseResampleFiles <- "ensemblev2/resample/ds2/"
 embeddingsFile <- "adhoc/redemaluca/ds2/dados/oficial/ensemble/ds2_with_PCA_50.RData"
 
 source(file_path_as_absolute("ipm/experimenters.R"))
