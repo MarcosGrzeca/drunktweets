2b31641ddad58a10cd861bfa075622c56708ad4b
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-06-22 10:24:27 -0300

    Create ds2.R

---
+++ ensemblev3/run/newdl/ds2.R
@@ -0,0 +1,20 @@
+library(tools)
+
+baseResultsFiles <- "ensemblev3/resultados/exp3/newdl/"
+baseResampleFiles <- "ensemblev2/resample/exp3/"
+embeddingsFile <- "adhoc/redemaluca/ds2/dados/oficial/ensemble/ds2_with_PCA_50.RData"
+
+source(file_path_as_absolute("ipm/experimenters.R"))
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("baseline/dados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+
+#Section: Dados classificar
+dados <- getDadosChat()
+
+try({
+	save <- 1
+	maxlen <- 38
+	max_words <- 16615
+	source(file_path_as_absolute("ensemblev3/run/newdl/core.R"))
+})
