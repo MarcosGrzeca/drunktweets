5c98d1a58932d7a0ba18ca8219458761f45a2343
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-08-10 16:47:24 -0300

    Vamos

---
+++ svmoverdl/svmdl.R
@@ -0,0 +1,58 @@
+library(tools)
+library(rword2vec)
+library(lsa)
+library(readr)
+library(quanteda)
+library(caret)
+
+library(doMC)
+Cores <- 8
+registerDoMC(cores=Cores)
+
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("baseline/dados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+source(file_path_as_absolute("utils/resultadoshelper.R"))
+source(file_path_as_absolute("adhoc/quanteda/metrics.R"))
+
+treinarPoly <- function(data_train, resposta) {
+    fit <- train(
+            x = data_train,
+            y = resposta, 
+            method = "svmPoly", 
+            trControl = trainControl(method = "cv", number = 5, savePred=T))
+    return (fit)
+}
+
+set.seed(10)
+library(xgboost)
+
+resultados <- data.frame(matrix(ncol = 4, nrow = 0))
+names(resultados) <- c("Name", "Precision", "Recall")
+
+addRowSimple <- function(resultados, rowName, precision, recall) {
+  newRes <- data.frame(rowName, precision, recall)
+  rownames(newRes) <- rowName
+  names(newRes) <- c("Name", "Precision", "Recall")
+  newdf <- rbind(resultados, newRes)
+  return (newdf)
+}
+
+for (year in 1:10) {
+  load(embeddingsFile)
+  inTrain <- sample(1:nrow(X), floor(.80 * nrow(X)))
+
+  tam <- ncol(X) - 1
+  one_hot_train <- X[inTrain, 1:tam]
+  resposta <-  X[inTrain, ncol(X)]
+  
+  one_hot_test <- X[-inTrain, 1:tam]
+  resposta_test <-  X[-inTrain, ncol(X)]
+  
+  classifier <- treinarPoly(one_hot_train, resposta)
+
+  # out-of-sample accuracy
+  preds <- predict(classifier, one_hot_test)
+  resultados <- addRowSimple(resultados, "Com", round(precision(preds>.50, resposta_test) * 100,6), round(recall(preds>.50, resposta_test) * 100,6))
+  View(resultados)
+}
