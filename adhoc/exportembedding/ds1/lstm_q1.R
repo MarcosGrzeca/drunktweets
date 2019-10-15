2b97afad66a9dba10f1973d10cdd3fb467099277
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-10-14 21:03:02 -0300

    Update lstm_q1.R

--- adhoc/exportembedding/ds1/lstm_q1.R
+++ adhoc/exportembedding/ds1/lstm_q1.R
@@ -75,10 +75,21 @@ history <- model %>%
     validation_split = 0.2
   )
 
-# predictions <- model %>% predict(list(dados_test_sequence))
-# predictions2 <- round(predictions, 0
-# matriz <- confusionMatrix(data = as.factor(predictions2), as.factor(dados_test$resposta), positive="1")
-# resultados <- addRowAdpater(resultados, DESC, matriz)
+addRowAdpater <- function(resultados, baseline, matriz, ...) {
+  newRes <- data.frame(baseline, matriz$byClass["F1"] * 100, matriz$byClass["Precision"] * 100, matriz$byClass["Recall"] * 100)
+  rownames(newRes) <- baseline
+  names(newRes) <- c("Baseline", "F1", "Precision", "Recall")
+  newdf <- rbind(resultados, newRes)
+  return (newdf)
+}
+
+resultados <- data.frame(matrix(ncol = 4, nrow = 0))
+names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
+
+predictions <- model %>% predict(list(dados_test_sequence))
+predictions2 <- round(predictions, 0)
+matriz <- confusionMatrix(data = as.factor(predictions2), as.factor(dados_test$resposta), positive="1")
+resultados <- addRowAdpater(resultados, "TESTE", matriz)
 
 ##
 library(dplyr)
