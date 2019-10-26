ee5955672eaa23b4b82900dd598d5d9a47433006
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-10-26 19:23:39 -0300

    Vamos lah

---
+++ embedding100/q2_redemaluca_with_PCA.R
@@ -0,0 +1,55 @@
+library(tools)
+library(rword2vec)
+library(lsa)
+library(readr)
+library(quanteda)
+
+library(doMC)
+Cores <- 8
+registerDoMC(cores=Cores)
+
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("baseline/dados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+source(file_path_as_absolute("utils/resultadoshelper.R"))
+source(file_path_as_absolute("adhoc/quanteda/metrics.R"))
+
+#Configuracoes
+DATABASE <- "icwsm"
+dados <- getDadosBaselineByQ("q2")
+
+fbcorpus <- corpus(dados$textEmbedding)
+fbdfm <- dfm(fbcorpus, remove=stopwords("english"), verbose=TRUE, remove_punct = TRUE)
+#fbdfm <- dfm_trim(fbdfm, min_docfreq = 2, verbose=TRUE)
+
+dados$entidades = gsub(",", " ", dados$entidades)
+entidades <- corpus(dados$entidades)
+entidadesdfm <- dfm(entidades, verbose=TRUE)
+
+dados$types = gsub(",", " ", dados$types)
+types <- corpus(dados$types)
+typesdfm <- dfm(types, verbose=TRUE)
+
+w2v <- readr::read_delim("adhoc/exportembedding/ds1/q2/cnn_10_epocas_10_filters164_100.txt", 
+                  skip=1, delim=" ", quote="",
+                  col_names=c("word", paste0("V", 1:100)))
+
+w2v <- w2v[w2v$word %in% featnames(fbdfm),]
+
+# creating new feature matrix for embeddings
+embed <- matrix(NA, nrow=ndoc(fbdfm), ncol=100)
+for (i in 1:ndoc(fbdfm)){
+  if (i %% 100 == 0) message(i, '/', ndoc(fbdfm))
+  vec <- as.numeric(fbdfm[i,])
+  doc_words <- featnames(fbdfm)[vec>0]
+  embed_vec <- w2v[w2v$word %in% doc_words, 2:101]
+  embed[i,] <- colMeans(embed_vec, na.rm=TRUE)
+# if no words in embeddings, simply set to 0
+  if (nrow(embed_vec)==0) embed[i,] <- 0
+}
+
+enriquecimento <- cbind(typesdfm, entidadesdfm)
+pca_entities <- prcomp(enriquecimento, scale = FALSE)
+
+X <- cbind(embed,pca_entities$x[,1:12], dados$resposta)
+save(X, file = "embedding100/ds1/q1_with_PCA_12.RData")
