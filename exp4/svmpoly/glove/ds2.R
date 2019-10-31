9542adce2bf50db74a75f0fd71c9f651183f6d28
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-10-31 15:43:54 -0300

    Glove maluco

---
+++ exp4/svmpoly/glove/ds2.R
@@ -0,0 +1,12 @@
+library(tools)
+
+source(file_path_as_absolute("ipm/experimenters.R"))
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("baseline/dados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+
+embeddingsFile <- "adhoc/redemaluca/ds2/dados/ds2_representacao_glove_PCA_50.RData"
+
+try({
+	source(file_path_as_absolute("exp4/svmpoly/svmvalidator.R"))
+})
---
+++ exp4/xgboost/glove/ds1q2.R
@@ -0,0 +1,12 @@
+library(tools)
+
+source(file_path_as_absolute("ipm/experimenters.R"))
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("baseline/dados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+
+embeddingsFile <- "adhoc/redemaluca/ds1/oficial/q2_glove_PCA12.RData"
+
+try({
+	source(file_path_as_absolute("exp4/xgboost/xgboost.R"))
+})
