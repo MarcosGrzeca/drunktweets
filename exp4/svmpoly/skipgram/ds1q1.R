8873b1e72628105f008acbff333a4e0b2a33f3c9
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-11-02 09:38:29 -0300

    bora

---
+++ exp4/svmpoly/skipgram/ds1q1.R
@@ -0,0 +1,12 @@
+library(tools)
+
+source(file_path_as_absolute("ipm/experimenters.R"))
+source(file_path_as_absolute("utils/getDados.R"))
+source(file_path_as_absolute("baseline/dados.R"))
+source(file_path_as_absolute("utils/tokenizer.R"))
+
+embeddingsFile <- "adhoc/redemaluca/ds1/q1_representacao_skip_PCA_10.RData"
+
+try({
+	source(file_path_as_absolute("exp4/svmpoly/svmvalidator.R"))
+})
