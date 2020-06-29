b0dc296bdd394125f32f39823268f3e612418339
Marcos Grzeça <marcos-a-g@hotmail.com>
2020-06-29 20:46:54 -0300

    Update graficoexp2.R

--- boxplot/graficoexp2.R
+++ boxplot/graficoexp2.R
@@ -8,6 +8,33 @@ data$classifier <-factor(data$classifier,levels=levels(data$classifier)[c(2,3,1,
 ggplot(data, aes(fill=classifier, y=f1_measure, x=dataset)) + 
   geom_bar(position="dodge", stat="identity") + theme_classic() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + scale_fill_brewer(palette="OrRd")
 
+ggplot(data, aes(fill=classifier, y=f1_measure, x=dataset)) + 
+  geom_bar(position="dodge", stat="identity") + theme_classic() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + scale_fill_grey(start = 0.9, end = 0.1) 
 
 ggplot(data, aes(fill=classifier, y=f1_measure, x=dataset)) + 
-  geom_bar(position="dodge", stat="identity") + theme_classic() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + scale_fill_grey(start = 0.8, end = 0.1) 
+  geom_bar(position="dodge", stat="identity") + theme_classic() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + scale_fill_brewer(palette="Blues")
+
+ggplot(data, aes(fill=classifier, y=f1_measure, x=dataset)) + 
+  geom_bar(position="dodge", stat="identity") + theme_classic() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + scale_fill_grey(start = 0.9, end = 0.1) 
+
+ggplot(data, aes(fill=classifier, y=f1_measure, x=dataset)) + 
+  geom_bar(position="dodge", stat="identity") + theme_classic() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + scale_fill_brewer(palette="BuPu")
+
+ggplot(data, aes(fill=classifier, y=f1_measure, x=dataset)) + 
+  geom_bar(position="dodge", stat="identity") + theme_classic() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + scale_fill_brewer(palette="PuBu")
+
+ggplot(data, aes(fill=classifier, y=f1_measure, x=dataset)) + 
+  geom_bar(position="dodge", stat="identity") + theme_classic() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + scale_fill_brewer(palette="Greys")
+
+ggplot(data, aes(fill=classifier, y=f1_measure, x=dataset)) + 
+  geom_bar(position="dodge", stat="identity") + theme_classic() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + scale_fill_brewer(palette="YlGn")
+
+ggplot(data, aes(fill=classifier, y=f1_measure, x=dataset)) + 
+  geom_bar(position="dodge", stat="identity") + theme_classic() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + scale_fill_brewer(palette="Purples")
+
+ggplot(data, aes(fill=classifier, y=f1_measure, x=dataset)) + 
+  geom_bar(position="dodge", stat="identity") + theme_classic() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + scale_fill_brewer(palette="Reds")
+
+#http://rstudio-pubs-static.s3.amazonaws.com/5312_98fc1aba2d5740dd849a5ab797cc2c8d.html
+
+#devtools::install_github("clauswilke/ggtextures")
